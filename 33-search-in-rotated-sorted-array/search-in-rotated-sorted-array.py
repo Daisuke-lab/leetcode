10b872ed7c0c5f2ffa1543ab4aae33ed77eccd58
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-22 11:00:40 -0500

    Time: 0 ms (100.00%) | Memory: 18.3 MB (23.16%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,17 +6,18 @@ class Solution:
         queue.append((i, j, False))
         while queue:
             l, r, pivot_found = queue.popleft()
+            next_pivot_found = pivot_found
             m = (l + r) // 2
             if nums[m] == target:
                 return m
             # pivot found
             if m < len(nums) - 1 and nums[m] > nums[m+1]:
-                pivot_found = True
+                next_pivot_found = True
             elif m > 0 and nums[m-1] > nums[m]:
-                pivot_found = True
-            if l <= m - 1:
-                queue.append((l, m-1, pivot_found))
-            if r >= m + 1:
-                queue.append((m+1, r, pivot_found))
+                next_pivot_found = True
+            if (l <= m - 1) and (pivot_found is False or target < nums[m]):
+                queue.append((l, m-1, next_pivot_found))
+            if r >= m + 1 and (pivot_found is False or target > nums[m]):
+                queue.append((m+1, r, next_pivot_found))
 
         return -1
