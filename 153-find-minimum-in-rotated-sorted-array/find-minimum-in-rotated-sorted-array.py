63d268b91b809914b40f3a432252419f54a27adf
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-22 11:47:40 -0500

    Time: 3 ms (3.37%) | Memory: 18 MB (50.74%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,26 +1,15 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l = 0
-        r = len(nums) - 1
-        self.nums = nums
-        while l <= r:
-            m = (l + r) // 2
-            if m == l and m == r:
-                return nums[m]
-            # It means right side has pivot, and min value is around pivot
-            elif self.in_left_sorted(l, m) and not self.in_right_sorted(m, r):
-                l = m + 1
-            # It means left side has pivot, and min value is around pivot
-            elif not self.in_left_sorted(l, m) and self.in_right_sorted(m, r):
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l) // 2
+            # If right side is sorted. the answer is always left side in spite of that left is sorted or owns pivot 
+            if nums[m] < nums[r]:
                 r = m
-            # the portion of list is already sorted. you can return the most left value as min
+            # It right side is not sorted, it means right side has pivot.
             else:
-                return nums[l]
+                l = m + 1
+        return nums[l]
 
 
-    def in_left_sorted(self, l, m):
-        return self.nums[l] <= self.nums[m]
-    def in_right_sorted(self, m, r):
-        return self.nums[m] <= self.nums[r]
-        
         
