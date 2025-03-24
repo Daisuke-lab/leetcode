45f440bf92c8ea8639ce8d479d680623314ca07b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-24 11:29:31 -0500

    Time: 7 ms (97.93%) | Memory: 18.2 MB (16.90%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -7,30 +7,12 @@
 class Solution:
     # If root.val == subRoot.val => you want to continue to check if it's the same tree (isSameTree)
     # If root.val != subRoot.val => you want to check with left and right
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        if root == None:
+            return "$#"
+
+        return ("$" + str(root.val) + self.serialize(root.left) + self.serialize(root.right))  
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if root is None and subRoot is None:
-            return True
-        elif root is None:
-            return False
-        elif subRoot is None:
-            return False
-        
-        if root.val == subRoot.val:
-            result = self.isSameTree(root, subRoot)
-            if result:
-                return True
-        
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
-
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is None:
-            return True
-        elif p is None:
-            return False
-        elif q is None:
-            return False
-        else:
-            if p.val != q.val:
-                return False
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        serialized_root = self.serialize(root)
+        serialized_sub_root = self.serialize(subRoot)
+        return serialized_sub_root in serialized_root
