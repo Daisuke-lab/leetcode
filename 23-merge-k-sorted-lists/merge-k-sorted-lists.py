f9c85788961ce10523076750cd485cb1f84f90e6
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-24 10:12:36 -0500

    Time: 8 ms (79.29%) | Memory: 20.1 MB (68.20%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -7,19 +7,46 @@ class Solution:
     # O(n*m) n: the length of list, m: the length of node
     # O(n) + O(logn*m), O(n): heapify, O(logm*n): m*n=total nodes => you add value to heap by log
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = head = ListNode()
-        heap = []
-        for i, node in enumerate(lists):
-            if node:
-                # If node.val is exactly same, i is used for sorting, which doesn't matter
-                heapq.heappush(heap, (node.val, i, node))
-
-        while heap:
-            val, _, node = heapq.heappop(heap)
-            head.next = node
-            head = node
-
-            node = node.next
-            if node:
-                heapq.heappush(heap, (node.val, _, node))
+        merged_lists = lists
+        if len(lists) == 0 or lists is None:
+            return None
+        while len(merged_lists) != 1:
+            new_merged_lists = []
+            for i in range(0, len(merged_lists), 2):
+                node1 = merged_lists[i]
+                node2 = merged_lists[i+1] if i+1 < len(merged_lists) else None
+                merged_node = self.mergeTwo(node1, node2)
+                new_merged_lists.append(merged_node)
+            merged_lists = new_merged_lists
+        return merged_lists[0]
+    
+    def mergeTwo(self, list1, list2):
+        if list1 is None and list2 is None:
+            return None
+        elif list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        else:
+            head = dummy = ListNode()
+            while list1 or list2:
+                if list1 is None:
+                    head.next = list2
+                    list2 = None
+                    continue
+                if list2 is None:
+                    head.next = list1
+                    list1 = None
+                    continue
+                if list1.val < list2.val:
+                    _next = list1.next
+                    list1.next = None
+                    head.next = list1
+                    list1 = _next
+                else:
+                    _next = list2.next
+                    list2.next = None
+                    head.next = list2
+                    list2 = _next
+                head = head.next
         return dummy.next
