ce832a32cdea28ab7894e8fb803002c71d1fc8f2
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-24 19:38:08 -0500

    Time: 182 ms (13.88%) | Memory: 19.1 MB (90.03%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -15,35 +15,25 @@ class Solution:
         for task in tasks:
             count_map[task] += 1
         heap = []
-        _ = -len(count_map) - 1
         for task, count in count_map.items():
-            _ += 1
-            heap.append((-count, _, task))
+            heap.append((-count, task))
         
         heapq.heapify(heap)
         intervals = 0
-        waiting = 0
-        interval_map = {}
         queue = collections.deque()
         while heap or queue:
 
             intervals += 1
             if queue and intervals - queue[0][1] > n:
-                heapq.heappush(heap, queue.popleft())
+                count, _, task = queue.popleft()
+                heapq.heappush(heap, (count, task))
             if len(heap) == 0:
                 continue
-
-            #print(interval_map, intervals, heapq.nsmallest(1, heap)[0])
-            count, _, task = heapq.nsmallest(1, heap)[0]
-            if task in interval_map and (intervals - interval_map[task]) <= n:
-                continue
-            count, _, task = heapq.heappop(heap)
-            interval_map[task] = intervals
+            count, task = heapq.heappop(heap)
             # because it's max heap and count is treated as negative, it's +1
             count += 1
             if count < 0:
                 queue.append((count, intervals, task))
-            #waiting = queue[0]
             
         return intervals
             
