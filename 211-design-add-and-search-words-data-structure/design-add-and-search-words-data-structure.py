b51b5428574b75b0d264eba1941bb31472771a17
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-25 19:46:36 -0500

    Time: 2236 ms (9.14%) | Memory: 67.9 MB (19.22%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,49 @@
+class Trie:
+    def __init__(self, c):
+        self.c = c
+        self.is_end = False
+        self.next = {}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Trie("")
+
+    def addWord(self, word: str) -> None:
+        self.add(word, self.root)
+
+    def add(self, word, node):
+        if word == "":
+            node.is_end = True
+            return
+        if word[0] in node.next:
+            next_node = node.next[word[0]]
+        else:
+            next_node = Trie(word[0])
+            node.next[word[0]] = next_node
+        self.add(word[1:], next_node)
+        
+
+    def search(self, word: str) -> bool:
+        return self.research(word, self.root)
+    def research(self, word, node):
+        if word == "":
+            if node.is_end:
+                return True
+            else:
+                return False
+        if word[0] in node.next:
+            return self.research(word[1:], node.next[word[0]])
+        elif word[0] == ".":
+            result = False
+            for next_node in node.next.values():
+                result = self.research(word[1:], next_node) or result
+            return result
+        else:
+            return False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
