47b3d3495b68cfec62e98fb6a7729d1bb026fda1
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-26 19:54:18 -0500

    Time: 11 ms (15.72%) | Memory: 19.3 MB (51.39%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         self.ad_list = self.init_ad_list(prerequisites)
-        #print(self.ad_list)
         self.visited = set()
+        self.memo = {}
         for v in self.ad_list.keys():
             if v not in self.visited:
                 has_cycle = self.has_cycle(v, set())
                 if has_cycle:
                     return False
         return True
+        
 
 
     def has_cycle(self, start, visited):
         self.visited.add(start)
         if start in visited:
             return True
+        if start in self.memo:
+            return self.memo[start]
         visited.add(start)
         for v in self.ad_list[start]:
             has_cycle = self.has_cycle(v, visited)
             if has_cycle:
                 visited.remove(start)
                 return True
-        self.ad_list[start] = []
+        # To speed up, when you don't find the cycle, you can make it no prerequisites.
+        #self.ad_list[start] = []
+        self.memo[start] = False
         visited.remove(start)
         return False
 
