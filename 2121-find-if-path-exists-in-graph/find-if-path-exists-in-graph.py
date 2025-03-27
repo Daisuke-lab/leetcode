b27fdd16a40edfbd6a4fb96b79fbb433f53264e6
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-27 15:31:02 -0500

    Time: 234 ms (97.43%) | Memory: 83.5 MB (94.33%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,26 +1,37 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1 for i in range(n)]
+
+    def find(self, v):
+        while self.parent[v] != v:
+            self.parent[v] = self.parent[self.parent[v]]
+            v = self.parent[v]
+        return v
+
+    def union(self, v1, v2):
+        v1_parent = self.find(v1)
+        v2_parent = self.find(v2)
+        if v1_parent == v2_parent:
+            return
+        v1_comp_size = self.size[v1_parent]
+        v2_comp_size = self.size[v2_parent]
+        if v1_comp_size > v2_comp_size:
+            self.parent[v2_parent] = v1_parent
+            self.size[v1_parent] += v2_comp_size
+        else:
+            self.parent[v1_parent] = v2_parent
+            self.size[v2_parent] += v1_comp_size
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        self.ad_list = self.init_ad_list(edges)
-        self.visited = set()
-        return self.dfs(source, destination)
+        union_find = UnionFind(n)
+        for v1, v2 in edges:
+            union_find.union(v1, v2)
+        source_parent = union_find.find(source)
+        dest_parent = union_find.find(destination)
+        return source_parent == dest_parent
         
 
-    def dfs(self, src, dest):
-        if src == dest:
-            return True
-        if src in self.visited:
-            return False
-        self.visited.add(src)
-        for v in self.ad_list[src]:
-            result = self.dfs(v, dest)
-            if result:
-                return True
-        return False
-
-    def init_ad_list(self, edges):
-        ad_list = defaultdict(list)
-        for v1, v2 in edges:
-            ad_list[v1].append(v2)
-            ad_list[v2].append(v1)
-        return ad_list
         
