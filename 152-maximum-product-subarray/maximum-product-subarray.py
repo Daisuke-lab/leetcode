2262507573c7fe24ceb466bddbe0bf3b455c65e9
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-29 14:52:02 -0500

    Time: 7 ms (90.53%) | Memory: 18.5 MB (36.97%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,23 +1,10 @@
 class Solution:
-    # Because there is a negative value and it's mulitply not add, so you can not use kadane
-    # Sliding Window?
-    # Brute Force
-    # n^2 0 <= i < len(nums), i <= j < len(nums) when i ==j, don't multiply
-    # because you need to check all i and j, no meaning to cache
-    # i is the always the start and j is always the end 
-    # you can check 3 (i+1) excluding i => n^2 /2
     def maxProduct(self, nums: List[int]) -> int:
-        min_product = 1
-        max_product = 1
-        answer = -float("inf")
-        for num in nums:
-            new_product1 = min_product * num
-            new_product2 = max_product * num
-            min_product = min(new_product1, new_product2, num)
-            max_product = max(new_product1, new_product2, num)
-            if max_product > answer:
-                answer = max_product
+        n, res = len(nums), nums[0]
+        prefix = suffix = 0
 
-            
-        return answer
-        
+        for i in range(n):
+            prefix = nums[i] * (prefix or 1)
+            suffix = nums[n - 1 - i] * (suffix or 1)
+            res = max(res, max(prefix, suffix))
+        return res
