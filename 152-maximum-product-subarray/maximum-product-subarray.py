88089351a012ffebf4180571e9449145d867944c
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-29 15:27:32 -0500

    Time: 9 ms (63.99%) | Memory: 18.4 MB (53.63%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,29 +1,36 @@
 class Solution:
-    # Let's assume that we have no "0"
-    # If you have even numbers of negative integers
-    # => just multiply everything
-    # If you have odd numbers of negative integers
-    # => you can split the array into two both having even number of negative integers, excluding the middle negative number
-    #  | 
-    # \_/
-    # So simply check from the both directions, either of direction find the max product
-    # You can check from both directions even when you have even number of negative integers although it is not necessary
-
-    # Ok. now what if you have 0?
-    # you can think of 0 as splitter as well.
-    # you split array into two and check from both directions
-    # ------------------>       -----> ------> ----->
-    # [1,2,0,3,4,5,0,9,9]    => [1,2], [3,4,5], [9,9]
-    # <------------------       <----  <------ <-----
-    # But practically you don't have to split your array. you can just check from both directions in one array
-    # but don't forget you reset the product with "0"
     def maxProduct(self, nums: List[int]) -> int:
-        n, res = len(nums), nums[0]
-        prefix = suffix = 0
-        for i in range(n):
-            print("suffix:", suffix, (suffix or 1))
-            prefix = nums[i] * (prefix or 1)
-            suffix = nums[n - 1 - i] * (suffix or 1)
-            res = max(res, max(prefix, suffix))
-        print(res)
-        return res
+        subarrays = []
+        # split by 0
+        cur = []
+        max_product = float('-inf')
+
+        for num in nums:
+            max_product = max(max_product, num)
+            if num == 0:
+                if cur:
+                    subarrays.append(cur)
+                cur = []
+            else:
+                cur.append(num)
+        if cur: subarrays.append(cur)
+
+        for sub in subarrays:
+            negs_count = sum(1 for i in sub if i < 0)
+            prod = 1
+            needed_negs_count = negs_count if negs_count % 2 == 0 else negs_count - 1
+            negs = 0
+            j = 0
+
+            for i in range(len(sub)):
+                prod *= sub[i]
+                if sub[i] < 0:
+                    negs += 1
+                    while negs > needed_negs_count:
+                        prod //= sub[j]
+                        if sub[j] < 0:
+                            negs -= 1
+                        j += 1
+                if j <= i:
+                    max_product = max(max_product, prod)
+        return max_product
