09315ed7090dbdfc07c9118d7149333ca135ca37
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-03-30 13:37:44 -0500

    Time: 3 ms (68.98%) | Memory: 17.7 MB (87.56%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,7 +1,4 @@
-class Trie:
-    def __init__(self):
-        self.next = {}
-        self.is_end = False
+
 
 
 class Solution:
@@ -11,40 +8,20 @@ class Solution:
     # self.memo = {word: True}
     # I want to make trie
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        self.root = self.build_trie(wordDict)
-        self.memo = {}
-        return self.dp(s)
-
-    def dp(self, s):
-        head = self.root
-        if s == "":
-            return False
-        if s in self.memo:
-            return self.memo[s]
-        result = False
-        for i in range(len(s)):
-            c = s[i]
-            if c not in head.next:
-                break
-            head = head.next[c]
-            if head.is_end:
-                result = self.dp(s[i+1:]) if i + 1 < len(s) else True
-                if result:
+        tab = [False for i in range(len(s) + 1)]
+        tab[-1] = True
+        for i in range(len(s) - 1, -1, -1):
+            for word in wordDict:
+                curr_length = len(s) - i + 1
+                curr_word = s[i:i+len(word)]
+                if curr_length < len(word):
+                    continue
+                if curr_word == word:
+                    tab[i] = tab[i + len(word)]
+                if tab[i]:
                     break
-        self.memo[s] = result
-        return result
-
-    def build_trie(self, words):
-        root = head = Trie()
-        for word in words:
-            head = root
-            for i,c in enumerate(word):
-                if c not in head.next:
-                    head.next[c] = Trie()
-                head = head.next[c]
-                if i == len(word) -1:
-                    head.is_end = True
-        return root
+        return tab[0]
+                
 
 
 
