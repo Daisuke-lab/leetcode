5d0c30b2e994aecc518a204282007e9df04aef5a
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-05 19:50:34 -0500

    Time: 45 ms (19.57%) | Memory: 17.9 MB (36.59%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,7 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        bin_n = bin(n)[2:]
-        fills = 32 - len(bin_n)
-        bin_n = "0"*fills + bin_n
-        return int(bin_n[::-1], 2)
+        res = 0
+        for i in range(32):
+            res = (res << 1) + (n&1)
+            n>>=1
+        return res
         
