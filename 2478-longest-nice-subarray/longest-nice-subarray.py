68583c113bcb6dadb90529690bf2c2f1166e5d76
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-07 17:47:48 -0500

    Time: 115 ms (23.36%) | Memory: 33.7 MB (14.16%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -7,18 +7,12 @@ class Solution:
         or_num = 0
         for i in range(len(nums)):
             # no overlap for 1 position
-            if or_num & nums[i] == 0:
-                # keep all one position with OR
-                or_num = or_num | nums[i]
-                stack_queue.append(nums[i])
-            else:
-                max_length = max(max_length, len(stack_queue))
-                while or_num & nums[i] != 0:
-                    removing_num = stack_queue.pop(0)
-                    or_num = or_num ^ removing_num
-                or_num = or_num | nums[i]
-                stack_queue.append(nums[i])
-        max_length = max(max_length, len(stack_queue))
+            while or_num & nums[i] != 0:
+                removing_num = stack_queue.pop(0)
+                or_num = or_num ^ removing_num
+            or_num = or_num | nums[i]
+            stack_queue.append(nums[i])
+            max_length = max(max_length, len(stack_queue))
         return max_length
                 
             
