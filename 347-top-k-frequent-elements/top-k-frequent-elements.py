fd829cd6b82a3af95f5420643dfa73bea65b686e
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-07 16:48:57 -0500

    Time: 3 ms (90.90%) | Memory: 21 MB (89.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,21 @@
 class Solution:
+    # Hashmap: n + nlogn
+    # min Heap with size k 3n
+    # 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count_map = defaultdict(int)
         for num in nums:
             count_map[num] += 1
-        heap = []
+        count_map = dict(sorted(count_map.items(), key = lambda item: item[1], reverse=True))
+        answer = []
         for num, count in count_map.items():
-            heap.append((-count, num))
-        heapq.heapify(heap)
-        return [num for count, num in heapq.nsmallest(k, heap)]
-        
+            k -= 1
+            if k >= 0:
+                answer.append(num)
+            if k ==0:
+                return answer
+            
+
 
 
         
