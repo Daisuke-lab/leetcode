acc2ddbc27025861ecc0807952a81081969220bb
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-08 14:51:54 -0500

    Time: 1130 ms (83.63%) | Memory: 17.5 MB (99.77%) - LeetSync

--- 3808-longest-palindrome-after-substring-concatenation-ii/longest-palindrome-after-substring-concatenation-ii.py
+++ 3808-longest-palindrome-after-substring-concatenation-ii/longest-palindrome-after-substring-concatenation-ii.py
@@ -6,62 +6,64 @@ class Solution:
             if list(s_set)[0] == list(t_set)[0]:
                 return len(s) + len(t)
 
-        def max_palindrome(arr, pos, even_odd):
-            if even_odd == 0: # Even case
-                l, r = pos, pos+1
-            else:
-                l, r = pos, pos
-
-            min_l = max_r = pos
-            while l >= 0 and r < len(arr):
-                if arr[l] == arr[r]:
-                    min_l = l
-                    max_r = r
-                else:
-                    break
-                l -= 1
-                r += 1
-            return min_l, max_r
-
-        n, m = len(s), len(t)
         # Find longest palindrome starting from each pos
-        s_pal = [1] * n
-        for i in range(n):
-            even_l, even_r = max_palindrome(s, i, 0)
-            even_len = even_r - even_l + 1
-            s_pal[even_l] = max(s_pal[even_l], even_len)
-            
-            odd_l, odd_r = max_palindrome(s, i, 1)
-            odd_len = odd_r - odd_l + 1
-            s_pal[odd_l] = max(s_pal[odd_l], odd_len)
-
-        t_pal = [1] * m
-        for j in range(m):
-            even_l, even_r = max_palindrome(t, j, 0)
-            even_len = even_r - even_l + 1
-            t_pal[even_r] = max(t_pal[even_r], even_len)
-
-            odd_l, odd_r = max_palindrome(t, j, 1)
-            odd_len = odd_r - odd_l + 1
-            t_pal[odd_r] = max(t_pal[odd_r], odd_len)
+        s_pal = self.calc_pal(s, is_s=True)
+        t_pal = self.calc_pal(t, is_s=False)
 
         ans = 1
-        for s_pos in range(n):
-            for t_pos in range(m-1, -1, -1):
+        for s_pos in range(len(s)):
+            for t_pos in range(len(t)-1, -1, -1):
                 # Update ans with palindrome from s or t only
                 ans = max(ans, s_pal[s_pos], t_pal[t_pos])
 
                 s_l, t_r = s_pos, t_pos
-                while s_l < n and t_r >= 0:
+                while s_l < len(s) and t_r >= 0:
                     if s[s_l] == t[t_r]:
                         s_l += 1
                         t_r -= 1
                         common_len = s_l - s_pos
                         ans = max(ans, 2 * common_len)
-                        if s_l < n:
+                        if s_l < len(s):
                             ans = max(ans, 2 * common_len + s_pal[s_l])
                         if t_r >= 0:
                             ans = max(ans, 2 * common_len + t_pal[t_r])
                     else:
                         break
         return ans
+
+    def calc_pal(self, string, is_s):
+        n = len(string)
+        pal = [1] * n
+        for i in range(n):
+            even_l, even_r = self.max_palindrome(string, i, is_even=True)
+            even_len = even_r - even_l + 1
+            if is_s:
+                pal[even_l] = max(pal[even_l], even_len)
+            else:
+                pal[even_r] = max(pal[even_r], even_len)
+        
+            odd_l, odd_r = self.max_palindrome(string, i, is_even=False)
+            odd_len = odd_r - odd_l + 1
+            if is_s:
+                pal[odd_l] = max(pal[odd_l], odd_len)
+            else:
+                pal[odd_r] = max(pal[odd_r], odd_len)
+        return pal
+
+
+    def max_palindrome(self, arr, pos, is_even):
+        if is_even: # Even case
+            l, r = pos, pos+1
+        else:
+            l, r = pos, pos
+
+        min_l = max_r = pos
+        while l >= 0 and r < len(arr):
+            if arr[l] == arr[r]:
+                min_l = l
+                max_r = r
+            else:
+                break
+            l -= 1
+            r += 1
+        return min_l, max_r
