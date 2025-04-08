7bb0ceebfc1992d488eba35d74b3bd6f1329492b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-08 14:21:24 -0500

    Time: 139 ms (42.42%) | Memory: 17.7 MB (88.12%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,40 +1,20 @@
 class Solution:
-    # You can expand the palindrome size from 1 to n
-    # You can return the size accordingly
-    # you keep adding the child size to your current size
-    # how can you avoid double count ?? => hashset
-    # you can speed up is_palindrome by cache
+    # Again. start from center
+    # Because everytime, you select a different center, it's going to be a different answer
     def countSubstrings(self, s: str) -> int:
-        self.memo = {}
-        self.counted = set()
-        self.s = s
-        return self.dp(0)
-
-    def dp(self, i):
-        if i in self.counted:
-            return 0
-        self.counted.add(i)
         count = 0
-        for k in range(i, len(self.s)+1):
-            substring = self.s[i:k]
-            if self.is_palindrome(substring):
-                #print(substring)
+        for i in range(len(s)):
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 count += 1
-            count += self.dp(k)
+                l -= 1
+                r += 1
+        for i in range(len(s) -1):
+            l = i
+            r = i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
         return count
-
-    def is_palindrome(self, s):
-        if len(s) == 0:
-            return False
-        if len(s) == 1:
-            return True
-        if len(s) == 2 and s[0] == s[-1]:
-            return True
-        if s in self.memo:
-            return self.memo[s]
-        result = False
-        if s[0] == s[-1]:
-            result = self.is_palindrome(s[1:-1])
-        self.memo[s] = result
-        return result
         
