75fa1532964bd6e306c9aadc913d48a5f526bf9b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-08 13:38:12 -0500

    Time: 122 ms (6.73%) | Memory: 40.6 MB (5.13%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,41 +1,44 @@
 class Solution:
-    # first you can make all substring n^2
-    # and then you can check if it's palindrome O(n)
-    # => NO
-
-    # you want to have answer list variable
-    # you want to maintain the size
-    # parition s[size*rep:size*(rep + 1)]
-    # if it's not palindrome, 
-    # the answer is the list of list, so you want to use top down
-    # then difine another function 
-    # What is the base case ?? => if s=="", it's the time when you add answer
-    # as args, you want to pass curr
-    # n^3
+    # Brute Force => n^3 <= n^2 for iteration, n for checking if it's palindrome
+    # odd palindrome n^2
+    # even palindrome n^2
+    # in total , it's 2n^2
+    # But it's hard to separate
+    # it's the list of list, so top down
+    
+    # iterate s and cut s and solve sub problem O(n)
+    # for each cutted s, you check if it's palindrome O(n)
+    # you are going to add current palindrome to answer O(n)
+    # what do you want to memo?
+    # memo[i] = palindrome[]
+    # then it can not be top down
+    # 
     def partition(self, s: str) -> List[List[str]]:
-        self.answer = []
-        self.recursion(s, [])
-        return self.answer
+        self.answers = []
+        self.s = s
+        return self.recursion(0)
+        
+    def recursion(self, i):
+        answer = []
+        for j in range(i, len(self.s)+1):
+            if self.is_palindrome(self.s[i:j]):
+                if j == len(self.s):
+                    answer.append([self.s[i:j]])
+                sub_answers = self.recursion(j)
+                for sub_answer in sub_answers:
+                    sub_answer.insert(0, self.s[i:j])
+                    answer.append(sub_answer)
+        return answer
 
 
-    def recursion(self, s, curr):
-        if s == "":
-            self.answer.append(curr.copy())
-        size = 1
-        while size <= len(s):
-            batch = s[:size]
-            if self.is_palindrome(batch):
-                curr.append(batch)
-                next_s = s[size:] if size != len(s) else ""
-                self.recursion(next_s, curr)
-                curr.pop()
-            size += 1
-            
 
     def is_palindrome(self, s):
         if s == "":
+            return False
+        elif len(s) == 1:
             return True
-        if s[0] == s[-1]:
+        elif len(s) == 2 and s[0] == s[-1]:
+            return True
+        elif s[0] == s[-1]:
             return self.is_palindrome(s[1:-1])
-        else:
-            False
+        return False
