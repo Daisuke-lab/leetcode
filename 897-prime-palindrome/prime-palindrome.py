7c108bbab7ae4ea2997f227bf03b822f16a4e903
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-09 12:09:09 -0500

    Time: 30 ms (86.08%) | Memory: 18.1 MB (11.34%) - LeetSync

--- 897-prime-palindrome/prime-palindrome.py
+++ 897-prime-palindrome/prime-palindrome.py
@@ -1,13 +1,27 @@
 class Solution:
-    def primePalindrome(self, N: int) -> int:
-        def isPrime(x):
-            if x < 2 or x % 2 == 0: return x == 2
-            for i in range(3, int(x**0.5) + 1, 2):
-                if x % i == 0: return False
+    def primePalindrome(self, n: int) -> int:
+        if 8 <= n <= 11:
+            return 11
+
+        half_size = len(str(n)) // 2
+        for x in range(10 ** half_size, 10**5):
+            #print(x)
+            palindrome = int(str(x) + str(x)[-2::-1])
+            #print(palindrome)
+            if palindrome >= n and self.isPrime(palindrome):
+                return palindrome
+
+                
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        if n <= 3:
             return True
-        if 8 <= N <= 11: return 11
-        for x in range(10 ** (len(str(N)) // 2), 10**5):
-            y = int(str(x) + str(x)[-2::-1])
-            if y >= N and isPrime(y): return y
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        for i in range(5, int(math.isqrt(n)) + 1, 6):
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+        return True
 
         
