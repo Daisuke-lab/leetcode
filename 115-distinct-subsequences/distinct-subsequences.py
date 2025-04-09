7a773eb74efab6b7e2dff4325578db1914c5851d
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-09 16:54:18 -0500

    Time: 475 ms (44.46%) | Memory: 74.9 MB (34.03%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,26 +1,12 @@
 class Solution:
-    # you have 2 patterns. Do you include current character or not?
-    # it's O(2^n)
-    
-    # you can decrese this O(m*n)
-    # you can create memo[i][j] *i=s's index, j=t's index
-    # you want to memoize the "count"
-    # the base case could be s="", t="", it is 1
-    # 
     def numDistinct(self, s: str, t: str) -> int:
-        if len(s) < len(t):
-            return 0
-        self.ROW = len(s)
-        self.COL = len(t)
-        memo = [[0 for j in range(self.COL)] for i in range(self.ROW)]
+        grid = [[0 if j != len(t) else 1 for j in range(len(t) + 1)] for i in range(len(s) + 1)]
+        
+        for i in range(len(s) -1, -1, -1):
+            for j in range(len(t)-1, -1, -1):
+                if s[i] == t[j]:
+                    grid[i][j] = grid[i+1][j+1] + grid[i+1][j]
+                else:
+                    grid[i][j] = grid[i+1][j]
 
-        for j in range(self.COL -1, -1, -1):
-            for i in range(self.ROW -1, -1, -1):
-                if i != self.ROW -1:
-                    memo[i][j] = memo[i+1][j]        
-                if j == self.COL - 1 and s[i] == t[j]:
-                    memo[i][j] += 1
-                if i != self.ROW - 1 and j != self.COL - 1 and memo[i+1][j+1] != 0 and s[i] == t[j]:
-                    memo[i][j] += memo[i+1][j+1]
-        #print(memo)
-        return memo[0][0]    
+        return grid[0][0]
