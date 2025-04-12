7f164b55d9536e12c3cc11533f3083cefb216b41
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-12 14:13:41 -0500

    Time: 111 ms (98.22%) | Memory: 39.7 MB (31.48%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,26 +1,14 @@
 class Solution:
-    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # you want to take an action when some cars meet
-        # I want to make a for loop with miles range(target+1) => Really?
-        # for loop with time would be better.
-        # After 1 hour, you add speed to position. if car reaches target in the same hour
-        # If the order of cars change, you want to change the speed of faster car
-        # You have to keep track of the order, which is not efficient.
-        # 0. make a hashmap {position: speed}
-        # 1. you can sort first (descending)
-        # 2. add speed
-        # 3. if it's less than or equal to right side, change the right side speed
-        # 4. create a hashmap everytime in for loop
-        
-        # you need count
-
-        stack = []
-        cars = [(p, s) for p, s in zip(position, speed)]
-        cars.sort(reverse=True)
-        for pos, speed in cars:
-            time = (target - pos) / speed
-            if len(stack) == 0 or stack[-1] < time:
-                stack.append(time)
-        return len(stack) 
-        
+    def carFleet(self, target: int, positions: List[int], speeds: List[int]) -> int:
+        fleet = 0
+        curr_slowest_time = -float("inf")
+        cars = sorted(list(zip(positions, speeds)), key= lambda car: car[0], reverse=True)
+        for position, speed in cars:
+            time = (target - position) / speed
+            if time <= curr_slowest_time:
+                continue
+            else:
+                curr_slowest_time = time
+                fleet += 1
+        return fleet
         
