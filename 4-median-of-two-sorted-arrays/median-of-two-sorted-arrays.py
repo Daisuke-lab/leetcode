ee9df3b427d9c2a06bd4179c3d092e5c1f73e1c2
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-14 17:23:59 -0500

    Time: 4 ms (35.40%) | Memory: 18.1 MB (40.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -12,19 +12,13 @@ class Solution:
         
 
         while True:
-            print("-----------------------")
             m = (l + r) // 2
-            print("m:", m)
             nums1_length = m + 1
             nums2_length = half_length - nums1_length
             left_partition_nums1_right_most = nums1[nums1_length-1] if nums1_length != 0 else -float("inf")
             left_partition_nums2_right_most = nums2[nums2_length-1] if nums2_length != 0 else -float("inf")
             right_partition_nums1_left_most = nums1[nums1_length] if nums1_length != len(nums1) else float("inf")
             right_partition_nums2_left_most = nums2[nums2_length] if nums2_length != len(nums2) else float("inf")
-            print("left partition nums1 right_most:", left_partition_nums1_right_most)
-            print("left partition nums2 right most:", left_partition_nums2_right_most)
-            print("right partition nums1 left most:", right_partition_nums1_left_most)
-            print("right partition nums2 left most:", right_partition_nums2_left_most)
 
             # nums1 partition is too big
             if left_partition_nums1_right_most > right_partition_nums2_left_most:
