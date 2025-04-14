e3d8f3c93cdee1f5cbc7fc4ae4f0f6d6e7de51cf
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-14 17:23:36 -0500

    Time: 11 ms (6.14%) | Memory: 18 MB (84.04%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -2,30 +2,41 @@ class Solution:
     
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums1, nums2 = [nums1, nums2] if len(nums1) <= len(nums2) else [nums2, nums1]
-        total = len(nums1) + len(nums2)
-        half = total // 2
-        is_even = total % 2 == 0
-
-
-        l, r = 0, len(nums1) - 1
-        while True:
-            i = (l + r) // 2
-            nums1_paritition_length = i + 1
-            j = (half - nums1_paritition_length) - 1
-
-            nums1left = nums1[i] if i >= 0 else float("-infinity")
-            nums1right = nums1[i + 1] if (i + 1) < len(nums1) else float("infinity")
-            nums2left = nums2[j] if j >= 0 else float("-infinity")
-            nums2right = nums2[j + 1] if (j + 1) < len(nums2) else float("infinity")
-
-            if nums1left <= nums2right and nums2left <= nums1right:
-                if is_even:
-                    return (max(nums1left, nums2left) + min(nums1right, nums2right)) / 2
-                else:
-                    return min(nums1right, nums2right)
+        l = 0
+        r = len(nums1) - 1
+        total_length = len(nums1) + len(nums2)
+        # If it's odd, you take bigger partition on left side.
+        half_length = total_length // 2
+        is_even = total_length % 2 == 0
                 
-            elif nums1left > nums2right:
-                r = i - 1
-            else:
-                l = i + 1
         
+
+        while True:
+            print("-----------------------")
+            m = (l + r) // 2
+            print("m:", m)
+            nums1_length = m + 1
+            nums2_length = half_length - nums1_length
+            left_partition_nums1_right_most = nums1[nums1_length-1] if nums1_length != 0 else -float("inf")
+            left_partition_nums2_right_most = nums2[nums2_length-1] if nums2_length != 0 else -float("inf")
+            right_partition_nums1_left_most = nums1[nums1_length] if nums1_length != len(nums1) else float("inf")
+            right_partition_nums2_left_most = nums2[nums2_length] if nums2_length != len(nums2) else float("inf")
+            print("left partition nums1 right_most:", left_partition_nums1_right_most)
+            print("left partition nums2 right most:", left_partition_nums2_right_most)
+            print("right partition nums1 left most:", right_partition_nums1_left_most)
+            print("right partition nums2 left most:", right_partition_nums2_left_most)
+
+            # nums1 partition is too big
+            if left_partition_nums1_right_most > right_partition_nums2_left_most:
+                r = m - 1
+                continue
+            # nums2 partition is too big
+            elif left_partition_nums2_right_most > right_partition_nums1_left_most:
+                l = m + 1
+                continue
+            if is_even:
+                small_num = max(left_partition_nums1_right_most, left_partition_nums2_right_most)
+                big_num = min(right_partition_nums1_left_most, right_partition_nums2_left_most)
+                return (small_num + big_num) / 2
+            else:
+                return min(right_partition_nums1_left_most, right_partition_nums2_left_most)
