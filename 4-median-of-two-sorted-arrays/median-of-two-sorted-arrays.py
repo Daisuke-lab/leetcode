953f7e268c6dceb309f950f3a6fbfc949258655e
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-14 17:06:07 -0500

    Time: 1 ms (65.51%) | Memory: 18.1 MB (40.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -4,12 +4,14 @@ class Solution:
         nums1, nums2 = [nums1, nums2] if len(nums1) <= len(nums2) else [nums2, nums1]
         total = len(nums1) + len(nums2)
         half = total // 2
+        is_even = total % 2 == 0
 
 
         l, r = 0, len(nums1) - 1
         while True:
             i = (l + r) // 2
-            j = half - i - 2
+            nums1_paritition_length = i + 1
+            j = (half - nums1_paritition_length) - 1
 
             nums1left = nums1[i] if i >= 0 else float("-infinity")
             nums1right = nums1[i + 1] if (i + 1) < len(nums1) else float("infinity")
@@ -17,9 +19,11 @@ class Solution:
             nums2right = nums2[j + 1] if (j + 1) < len(nums2) else float("infinity")
 
             if nums1left <= nums2right and nums2left <= nums1right:
-                if total % 2:
+                if is_even:
+                    return (max(nums1left, nums2left) + min(nums1right, nums2right)) / 2
+                else:
                     return min(nums1right, nums2right)
-                return (max(nums1left, nums2left) + min(nums1right, nums2right)) / 2
+                
             elif nums1left > nums2right:
                 r = i - 1
             else:
