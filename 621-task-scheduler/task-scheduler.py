941778e69531937276aeb452e313a64aa575345f
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-15 15:39:19 -0500

    Time: 174 ms (14.94%) | Memory: 19.1 MB (90.10%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,39 +1,24 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # you should execute a variety of tasks as many as possible because interfval is the same for all the task
-        # you want to start with the higher number of char
-        # Heap can be fast because the max number of tasks changes
-        # You also maintain the prev task => you need to use hashmap to maintain the last interval
-        # you also maintain the count for intervals
-        # you also need hashmap for initial count
-        # 1. create count_map O(n)
-        # 2. create heap O(n)
-        # 3. execute task (check interval and prev acoordingly)
-        # 4. insert a new (count, "A") O(logm) * n m: the apperarnce of a character
-        
-        count_map = defaultdict(int)
+        count_map = {}
         for task in tasks:
-            count_map[task] += 1
-        heap = []
-        for task, count in count_map.items():
-            heap.append((-count, task))
-        
-        heapq.heapify(heap)
-        intervals = 0
-        queue = collections.deque()
-        while heap or queue:
-
-            intervals += 1
-            if queue and intervals - queue[0][1] > n:
-                count, _, task = queue.popleft()
-                heapq.heappush(heap, (count, task))
-            if len(heap) == 0:
+            count_map[task] = count_map.get(task, 0) + 1
+        task_queue = [(-count, task) for task, count in count_map.items()]
+        heapq.heapify(task_queue)
+        waiting_queue = []
+        interval = 0
+        while task_queue or waiting_queue:
+            interval += 1
+            if waiting_queue and interval - waiting_queue[0][0] > n:
+                past_interval, count, task = waiting_queue.pop(0)
+                heapq.heappush(task_queue, (-count, task))
+            if not task_queue:
+                # idling
                 continue
-            count, task = heapq.heappop(heap)
-            # because it's max heap and count is treated as negative, it's +1
-            count += 1
-            if count < 0:
-                queue.append((count, intervals, task))
-            
-        return intervals
+            count, task = heapq.heappop(task_queue)
+            count *= -1
+            count -= 1
+            if count > 0:
+                waiting_queue.append((interval, count, task))
+        return interval
             
