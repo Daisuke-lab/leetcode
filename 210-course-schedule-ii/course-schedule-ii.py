6299e5370836bfc33780423626211e0bf6719f43
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-15 18:54:34 -0500

    Time: 7 ms (28.37%) | Memory: 19.2 MB (60.99%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -5,32 +5,27 @@ class Solution:
     # you can still clean up ads once you know it's doesn't have cycle because before it cleans up,it collects result
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         self.answer = []
+        self.learned = set()
         self.ad_list = self.init_ad_list(numCourses, prerequisites)
-        self.visited = set()
-        print(self.ad_list)
-        for v in self.ad_list.keys():
-            if v not in self.visited:
-                has_cycle = self.has_cycle(v, set())
-                if has_cycle:
+        for node in self.ad_list:
+            if node not in self.learned:
+                if self.dfs(node, set()):
                     return []
+        
+        return self.answer
 
-        return list(self.answer)
-    
-    def has_cycle(self, src, curr):
-        #print(src)
-        if src in curr:
+    def dfs(self, node, visited):
+        if node in visited:
             return True
-        curr.add(src)
-        has_cycle = False
-        for v in self.ad_list[src]:
-            has_cycle = self.has_cycle(v, curr)
-            if has_cycle:
+        visited.add(node)
+        for ad in self.ad_list[node]:
+            if self.dfs(ad, visited):
                 return True
-        if src not in self.visited:
-            self.answer.append(src)
-        self.visited.add(src)
-        curr.remove(src)
-        self.ad_list[src] = []
+        self.ad_list[node] = []
+        if node not in self.learned:
+            self.answer.append(node)
+            self.learned.add(node)
+        visited.remove(node)
         return False
 
     def init_ad_list(self, numCourses, edges):
