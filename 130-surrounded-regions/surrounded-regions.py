514489a90faff9e1f9ba611a92a90f96d619647d
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-15 17:50:12 -0500

    Time: 7 ms (55.49%) | Memory: 21.8 MB (53.50%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,42 +1,43 @@
 class Solution:
+
     def solve(self, board: List[List[str]]) -> None:
         self.board = board
         self.ROW = len(board)
         self.COL = len(board[0])
-        for j in range(self.COL):
-            first_row = 0
-            last_row = self.ROW - 1
-            if board[first_row][j] == "O":
-                self.dfs(first_row, j)
-            if board[last_row][j] == "O":
-                self.dfs(last_row, j)
-        
-        for i in range(self.ROW):
-            first_col = 0
-            last_col = self.COL - 1
-            if board[i][first_col] == "O":
-                self.dfs(i, first_col)
-            if board[i][last_col] == "O":
-                self.dfs(i, last_col)
+        self.check_safe_zone()
+        self.conquer()
+        return board
 
+    def conquer(self):
         for i in range(self.ROW):
             for j in range(self.COL):
-                if board[i][j] == "O":
-                    board[i][j] = "X"
-                elif board[i][j] == "T":
-                    board[i][j] = "O" 
-    
-    def dfs(self, i, j):
-        if i < 0 or i == self.ROW or j < 0 or j == self.COL:
-            return
-        if self.board[i][j] != "O":
-            return
-
-        self.board[i][j] = "T"
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for direction in directions:
-            new_i = i + direction[0]
-            new_j = j + direction[1]
-            self.dfs(new_i, new_j)
-
-                            
+                if self.board[i][j] == "O":
+                    self.board[i][j] = "X"
+                elif self.board[i][j] == "S":
+                    self.board[i][j] = "O"
+        
+        
+    def check_safe_zone(self):
+        queue = collections.deque()
+        for i in range(self.ROW):
+            if self.board[i][0] == "O":
+                queue.append((i, 0))
+            if self.board[i][self.COL -1] == "O":
+                queue.append((i, self.COL -1))
+        for j in range(self.COL):
+            if self.board[0][j] == "O":
+                queue.append((0, j))
+            if self.board[self.ROW-1][j] == "O":
+                queue.append((self.ROW-1, j))
+        while queue:
+            i, j = queue.popleft()
+            self.board[i][j] = "S"
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            for direction in directions:
+                next_i = i + direction[0]
+                next_j = j + direction[1]
+                if next_i < 0 or next_j < 0 or next_i == self.ROW or next_j == self.COL:
+                    continue
+                if self.board[next_i][next_j] == "O":
+                    queue.append((next_i, next_j))
+                                    
