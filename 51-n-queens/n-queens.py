e8f715e4549cada98c39ef05e95415a8110e10dc
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-15 16:10:57 -0500

    Time: 15 ms (42.97%) | Memory: 18.5 MB (15.74%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,48 +1,45 @@
 class Solution:
+    # Brute Force
+    # 1. Put one queen
+    # 2. mask overlapped position
+    # 3. recursion
+    # O(n^3)
+
+    # Clever solution
+    # 1. iterate 1 row, put queen anywhere
+    # 2. generate next row with masking
+    # 
+    # O(n^2)
+
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # queen is not vertically, horizontaly, diagonaly overlapped.
-        # One solution
-        # 1. you create a board
-        # 2. put queen in open space
-        # 3. out the space having conflict
-        # 4. call recursion and return the max queen number
-        # 5. base case is when all cells are out
-        # top down is more natural
-        # you want to pass positions=[(i,j)] and taken=[i, j, (i, j)] andas args
-        # *you can insert diagonal position to taken but taken is hard to be resued for the next iteration
-        # Let's define is_available function to check diagonal line 
-        # once you finish the recursion, populate the board
-        
-        # Put somewhere in the first row 
-        # the next row, you can not put the next queen in adjacent column in the previous queen
-        # maintain columns
         self.positions_list = []
         self.n = n
-        self.recursion(0, set(), set(), set(), [])
-        answer = []
-        return [["".join(["Q" if (i, j) in positions else "." for j in range(n)]) for i in range(n)] for positions in self.positions_list]
-        # for positions in self.positions_list:
-        #     answer.append(["".join(["Q" if (i, j) in positions else "." for j in range(n)]) for i in range(n)])
-        # return answer
-    
-    def recursion(self, i, taken_columns, left_invalid, right_invalid, curr):
+        self.recursion(set(), set(), set(), [], 0)
+        answer_list = []
+        for positions in self.positions_list:
+            answer = []
+            for i, j in positions:
+                s = "".join(["Q" if k == j else "." for k in range(n)])
+                answer.append(s)
+            answer_list.append(answer)
+        return answer_list
+
+
+    def recursion(self, out_col, out_dig_left, out_dig_right, positions, i):
         if i == self.n:
-            self.positions_list.append(curr.copy())
-            return
+            self.positions_list.append(positions.copy())
         for j in range(self.n):
-            if j in taken_columns:
-                continue
-            if j + i in left_invalid:
-                continue
-            if i - j in right_invalid:
-                continue
-            taken_columns.add(j)
-            curr.append((i, j))
-            left_invalid.add(j + i)
-            right_invalid.add(i - j)
-            self.recursion(i+1, taken_columns, left_invalid, right_invalid, curr)
-            taken_columns.remove(j)
-            curr.pop()
-            left_invalid.remove(j + i)
-            right_invalid.remove(i - j)
+            dig_left = i + j
+            dig_right = i - j
+            if j not in out_col and dig_left not in out_dig_left and dig_right not in out_dig_right:
+                positions.append((i, j))
+                out_col.add(j)
+                out_dig_left.add(dig_left)
+                out_dig_right.add(dig_right)
+                self.recursion(out_col, out_dig_left, out_dig_right, positions, i + 1)
+                positions.pop()
+                out_col.remove(j)
+                out_dig_left.remove(dig_left)
+                out_dig_right.remove(dig_right)
+
 
