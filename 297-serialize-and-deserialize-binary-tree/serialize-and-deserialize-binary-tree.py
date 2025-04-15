85e9251cf89f4d36e81a07ae82d392be4a22e3d1
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-14 19:31:22 -0500

    Time: 127 ms (14.54%) | Memory: 21.2 MB (78.42%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -8,63 +8,37 @@
 
 class Codec:
 
-    def to_map(self, root):
+    def to_list(self, root, curr):
         if root is None:
-            return {}
-        else:
-            return {"val": root.val, "left": self.to_map(root.left), "right": self.to_map(root.right)}
+            curr.append("N")
+            return curr
+        curr.append(root.val)
+        curr = self.to_list(root.left, curr)
+        curr = self.to_list(root.right, curr)
+        return curr
+
     def serialize(self, root):
-        return str(self.to_map(root))
+        return str(self.to_list(root,  []))
 
     def deserialize(self, data):
-        #print(data)
-        return self.to_tree(data)
-
-    def to_tree(self, data):
-        if data == "{" + "}":
-            return None
-        val, i = self.get_val(data)
-        left_data, i = self.get_next_data(data, i)
-        right_data, i = self.get_next_data(data, i)
-        node = TreeNode(val)
-        node.left = self.to_tree(left_data)
-        node.right = self.to_tree(right_data)
-        return node
-
-    def get_val(self, data):
-        val_prefix = "{'val': "
-        i = len(val_prefix)
-        str_num = ""
-        is_negative = False
-        while True:
-            try:
-                str_num += str(int(data[i]))
-                i += 1
-            except:
-                if data[i] == "-":
-                    is_negative = True
-                    i += 1
-                else:
-                    break
-        num = int(str_num)
-        num = -num if is_negative else num
-        return ( num, i )
-
-    def get_next_data(self, data, i):
-        while data[i] != "{":
-            i += 1
-        start = i
-        i += 1
-        open_count = 1
-        close_count = 0
-        while open_count != close_count:
-            if data[i] == "{":
-                open_count += 1
-            if data[i] == "}":
-                close_count += 1
-            i += 1
-        return (data[start:i], i)
+        data = data.replace("[", "").replace("]", "").replace("'", "").replace(" ", "")
+        nums = data.split(",")
+        return self.to_tree(nums)
         
+
+    def to_tree(self, nums):
+        val = nums.pop(0)
+        val = int(val) if val != "N" else None
+        if val is None:
+            return None
+        else:
+            root = TreeNode(val)
+            root.left = self.to_tree(nums)
+            root.right = self.to_tree(nums)
+            return root
+        #return self.to_tree(data)
+
+
                     
 
 # Your Codec object will be instantiated and called as such:
