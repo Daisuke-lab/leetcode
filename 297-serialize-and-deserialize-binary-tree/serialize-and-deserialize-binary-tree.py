3cd04aa01feeb16bc99cc33d48a14dc16d553dc5
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-14 19:04:23 -0500

    Time: 1624 ms (5.23%) | Memory: 38.3 MB (5.65%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -5,41 +5,66 @@
 #         self.left = None
 #         self.right = None
 
+
 class Codec:
 
-    def serialize(self, root):
-        self.serialized = []
-        self.preorder(root)
-        print(self.serialized)
-        return ",".join(self.serialized)
-
-
-    def preorder(self, root):
+    def to_map(self, root):
         if root is None:
-            self.serialized.append("N")
-            return
-        self.serialized.append(str(root.val))
-        self.preorder(root.left)
-        self.preorder(root.right)
-
-        
-        
+            return {}
+        else:
+            return {"val": root.val, "left": self.to_map(root.left), "right": self.to_map(root.right)}
+    def serialize(self, root):
+        return str(self.to_map(root))
 
     def deserialize(self, data):
-        self.vals = data.split(",")
-        self.i = 0
-        return self.dfs()
-    
-    def dfs(self):
-        if self.vals[self.i] == "N":
-            self.i += 1
+        #print(data)
+        return self.to_tree(data)
+
+    def to_tree(self, data):
+        if data == "{" + "}":
             return None
-        node = TreeNode(int(self.vals[self.i]))
-        self.i += 1
-        node.left = self.dfs()
-        node.right = self.dfs()
+        val, i = self.get_val(data)
+        left_data, i = self.get_next_data(data, i)
+        right_data, i = self.get_next_data(data, i)
+        node = TreeNode(val)
+        node.left = self.to_tree(left_data)
+        node.right = self.to_tree(right_data)
         return node
 
+    def get_val(self, data):
+        val_prefix = "{'val': "
+        i = len(val_prefix)
+        str_num = ""
+        is_negative = False
+        while True:
+            try:
+                str_num += str(int(data[i]))
+                i += 1
+            except:
+                if data[i] == "-":
+                    is_negative = True
+                    i += 1
+                else:
+                    break
+        num = int(str_num)
+        num = -num if is_negative else num
+        return ( num, i )
+
+    def get_next_data(self, data, i):
+        while data[i] != "{":
+            i += 1
+        start = i
+        i += 1
+        open_count = 1
+        close_count = 0
+        while open_count != close_count:
+            if data[i] == "{":
+                open_count += 1
+            if data[i] == "}":
+                close_count += 1
+            i += 1
+        return (data[start:i], i)
+        
                     
 
 # Your Codec object will be instantiated and called as such:
