03696d51fa5d2a38e6b3ac157baeb1394eee2fff
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-17 17:19:33 -0500

    Time: 35 ms (40.43%) | Memory: 19.4 MB (38.67%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,44 +1,31 @@
 class Solution:
     # 
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        INF = float("inf")
-        adj = [[] for _ in range(n)]
-        dist = [[INF] * (k + 2) for _ in range(n)]
-        for u, v, cst in flights:
-            adj[u].append([v, cst])
+        ad_list = self.init_ad_list(n, flights)
+        dj_map = [float("inf") for i in range(n)]
+        dj_map[src] = 0
+        for i in range(k+1):
+            new_dj_map = dj_map.copy()
+            for node in range(n):
+                if dj_map[node] == float("inf"):
+                    continue
+                spent = dj_map[node]
+                for ad, price in ad_list[node]:
+                    if new_dj_map[ad] > spent + price:
+                        new_dj_map[ad] = spent + price
+            dj_map = new_dj_map
+            #print(dj_map)           
         
-        dist[src][0] = 0
-        minHeap = [(0, src, -1)] # cost, node, stops
-        while len(minHeap):
-            cst, node, stops = heapq.heappop(minHeap)
-            if dst == node: return cst
-            if stops == k or dist[node][stops + 1] < cst:
-                continue
-            for nei, w in adj[node]:
-                nextCst = cst + w
-                nextStops = 1 + stops
-                if dist[nei][nextStops + 1] > nextCst:
-                    dist[nei][nextStops + 1] = nextCst
-                    heapq.heappush(minHeap, (nextCst, nei, nextStops))
+        
+        return dj_map[dst] if dj_map[dst] != float("inf") else - 1
 
-        return -1
         
 
         
         
 
-    def init_dj_map(self, ad_list):
-        dj_map = {}
-        for node in ad_list:
-            # price, prev_node
-            dj_map[node] = (float("inf"), None)
-        return dj_map
-
-    def init_ad_list(self, flights):
-        ad_list = {}
+    def init_ad_list(self, n, flights):
+        ad_list = [set() for i in range(n)]
         for src, dest, price in flights:
-            ads = ad_list.get(src, set())
-            ads.add((dest, price))
-            ad_list[src] = ads
-            ad_list[dest] = ad_list.get(dest, set())
-        return ad_list       
+            ad_list[src].add((dest, price))
+        return ad_list      
