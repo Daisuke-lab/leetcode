2276b59cf40ebf531a54d9d456b0f36931985823
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-16 19:43:39 -0500

    Time: 11 ms (56.99%) | Memory: 19.2 MB (47.43%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -3,7 +3,7 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         INF = float("inf")
         adj = [[] for _ in range(n)]
-        dist = [[INF] * (k + 5) for _ in range(n)]
+        dist = [[INF] * (k + 2) for _ in range(n)]
         for u, v, cst in flights:
             adj[u].append([v, cst])
         
