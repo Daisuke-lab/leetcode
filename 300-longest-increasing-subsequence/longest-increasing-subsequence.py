f48371c4c703f91f4abf6113df4d7ba88485a449
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-23 15:09:46 -0500

    Time: 99 ms (69.96%) | Memory: 18.6 MB (9.23%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,19 +1,54 @@
-class Solution:
-    # every time you have 2 choices either you include current value or not => 2^n
-    # you want to use DP
-    # how can you cache it??
-    # tab [max_length] => n^2
-    # how about top down
-    # you also have a chance to start subseqeuence at any position => O(n)
+class SegmentTree:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (2 * self.n)
 
+
+    def update(self, index, value):
+        # Set value at position index
+        i = index + self.n
+        self.tree[i] = value
+        i //= 2
+        while i > 0:
+            self.tree[i] = max(self.tree[i*2], self.tree[i*2+1])
+            i //= 2
+
+    def get_max(self, left, right):
+        # Sum on interval [left, right]
+        left += self.n
+        right += self.n
+        result = 0
+        while left <= right:
+            if left % 2 == 1:
+                result = max(result, self.tree[left])
+                left += 1
+            if right % 2 == 0:
+                result = max(result, self.tree[right])
+                right -= 1
+            left //= 2
+            right //= 2
+        return result
+class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        tab = [1 for i in range(len(nums))]
-        for i in range(len(nums) -1 , -1, -1):
-            j = i + 1
-            while j < len(nums):
-                if nums[i] < nums[j]:
-                    tab[i] = max(tab[i], tab[j]+1)
-                j += 1
-        return max(tab)
+        def compress(arr):
+            sortedArr = sorted(set(arr))
+            order = []
+            for num in arr:
+                order.append(bisect_left(sortedArr, num))
+            return order
+        
+        nums = compress(nums)
+        n = len(nums)
+        segTree = SegmentTree(n)
+
+        LIS = 0
+        for num in nums:
+            curLIS = segTree.get_max(0, num - 1) + 1
+            segTree.update(num, curLIS)
+            LIS = max(LIS, curLIS)
+        return LIS
+
+        
+        
             
         
