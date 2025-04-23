5bff25d72205faf630b93d88b132e6b410f53ebc
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-23 11:14:45 -0500

    Time: 1205 ms (10.90%) | Memory: 58.4 MB (5.95%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/range-sum-query-mutable.py
@@ -1,7 +1,6 @@
 class SegmentTree:
     def __init__(self):
         self.sum = 0
-        self.range = []
         self.left = None
         self.right = None
 
@@ -9,52 +8,47 @@ class SegmentTree:
         self.range = [i, j]
         if i == j:
             self.sum = nums[i]
-            return 
+            return self
+        if i > j:
+            return None
         m = (i + j) // 2
-        if i <= m:
-            self.left = SegmentTree()
-            self.left.build(nums, i, m)
-            self.sum += self.left.sum
-        if m + 1<= j:
-            self.right = SegmentTree()
-            self.right.build(nums, m+1, j)
-            self.sum += self.right.sum
-            
-
-    def sumRange(self, i, j):
-        if self.range[0] == i and self.range[1] == j:
-           return self.sum
-        # this is confirmed, because the range is definitely in the list
-        elif self.left.range[0] <= i and j <= self.left.range[1]:
-            return self.left.sumRange(i, j)
-        elif self.right.range[0] <= i and j <= self.right.range[1]:
-            return self.right.sumRange(i, j)
-        else:
-            left_i = i
-            left_j = self.left.range[1]
-            right_i = self.right.range[0]
-            right_j = j
-            return self.left.sumRange(left_i, left_j) + self.right.sumRange(right_i, right_j)
+        self.left = SegmentTree().build(nums, i, m)
+        self.right = SegmentTree().build(nums, m+1, j)
+        self.sum += self.left.sum if self.left else 0
+        self.sum += self.right.sum if self.right else 0
+        return self
 
     def update(self, i, val):
-        if self.range[0] == i and self.range[1] == i:
-            gap = val - self.sum
+        if i == self.range[0] and i == self.range[1]:
+            gap = self.sum - val
             self.sum = val
             return gap
-        elif self.left.range[0] <= i and i <= self.left.range[1]:
-            gap = self.left.update(i, val)
-        elif self.right.range[0] <= i and i <= self.right.range[1]:
+        if self.right and self.right.range[0] <= i and i <= self.right.range[1]:
             gap = self.right.update(i, val)
-        self.sum += gap
+        else:
+            gap = self.left.update(i, val)
+        self.sum -= gap
         return gap
-            
 
+    def sumRange(self, i, j):
+        if i == self.range[0] and j == self.range[1]:
+            return self.sum
+        if self.left and self.left.range[0] <= i and j <= self.left.range[1]:
+            return self.left.sumRange(i, j)
+        elif self.right and self.right.range[0] <= i and j <= self.right.range[1]:
+            return self.right.sumRange(i, j)
+        else:
+            left_j = self.left.range[1]
+            right_i = self.right.range[0]
+            return self.left.sumRange(i, left_j) + self.right.sumRange(right_i, j)
+             
 
 class NumArray:
 
     def __init__(self, nums: List[int]):
         self.root = SegmentTree()
         self.root.build(nums, 0, len(nums) - 1)
+        #print(self.root.sum)
         
 
     def update(self, index: int, val: int) -> None:
