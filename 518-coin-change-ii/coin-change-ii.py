0e0d0ce4d95178e28bf80f82514b4a74ec49681c
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-24 17:15:27 -0500

    Time: 541 ms (25.09%) | Memory: 62.5 MB (25.34%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,21 +2,13 @@ class Solution:
     # amount: count
     # 
     def change(self, amount: int, coins: List[int]) -> int:
-        self.coins = coins
-        self.memo = {}
-        return self.dp(0, amount)
-
-    def dp(self, i, amount):
-        if amount == 0:
-            return 1
-        if amount < 0:
-            return 0
-        if i == len(self.coins):
-            return 0
-        if (i, amount) in self.memo:
-            return self.memo[(i, amount)]
-        count = self.dp(i, amount-self.coins[i])
-        count += self.dp(i+1, amount)
-        self.memo[(i, amount)] = count
-        return count
+        grid = [[0 for j in range(len(coins))] for i in range(amount+1)]
+        grid[0] = [1 for j in range(len(coins))]
+        for i in range(1, amount+1):
+            for j in range(len(coins)):
+                coin = coins[j]
+                count_when_curr_used = grid[i - coin][j] if i - coin >= 0 else 0
+                count_when_curr_not_used = grid[i][j-1] if j - 1 >= 0 else 0
+                grid[i][j] = count_when_curr_used +count_when_curr_not_used
+        return grid[-1][-1]
                 
