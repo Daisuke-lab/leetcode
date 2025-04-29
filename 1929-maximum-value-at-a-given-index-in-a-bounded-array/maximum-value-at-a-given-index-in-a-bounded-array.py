cb5301eef602037a899b0e9cb59db695980149e8
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-28 23:05:46 -0500

    Time: 2 ms (81.16%) | Memory: 17.9 MB (23.80%) - LeetSync

---
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def test(self, m):
+            l = max(m - self.index, 0)
+            length = m - l + 1
+            res = length * (l + m) / 2
+            r = max(m - ((self.n - 1) - self.index), 0)
+            length = m - r + 1
+            res += length * (m + r) / 2
+            return res - m
+
+    def maxValue(self, n: int, index: int, maxSum: int) -> int:
+        self.n = n
+        self.index = index
+
+        maxSum -= n
+        left, right = 0, maxSum
+        while left < right:
+            mid = (left + right + 1) // 2
+            if self.test(mid) <= maxSum:
+                left = mid
+            else:
+                right = mid - 1
+        result = left + 1
+        return result
