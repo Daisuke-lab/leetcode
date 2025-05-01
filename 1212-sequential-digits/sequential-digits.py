3e39847d752fa38019946ba039deffbc68fca1c7
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-04-30 19:29:24 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (36.96%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -5,41 +5,71 @@ class Solution:
 
     # Btw, it returns the list and still uses DP
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        str_low = str(low)
-        str_high = str(high)
-        first_digit = str_low[0]
-        last_digit = str_high[:len(str_high) - len(str_low)+1]
-        digit_num = len(last_digit) - len(first_digit) + 1
-        first_digit = int(first_digit)
-        last_digit = int(last_digit)
-        start_digits = []
-        stop_sign = False
-        for i in range(1, digit_num+1):
-            for j in range(1, 10):
-                # 1,2,3,4,5,6,7,8,9
-                # 12, 23, 34, 45, 67
-                if j + i -1 >= 10:
-                    continue
-                start_digit = int("".join([str(j + k) for k in range(i)]))
-                if start_digit > last_digit:
-                    stop_sign = True
-                    break
-                if first_digit <= start_digit and start_digit <= last_digit:
-                    start_digits.append(start_digit)
-            if stop_sign:
-                break
+        self.answers = []
+        self.low = str(low)
+        self.high = str(high)
+        self.gap = len(self.high) - len(self.low)
+        # print("N:", len(self.high))
+        # print("GAP:",self.gap)
+        self.low = "0" * self.gap + self.low
+        low_tight = True if self.gap == 0 else False
+        high_tight = True
+        self.recursion(0, "", low_tight, high_tight)
+        return self.answers
+        
+    def recursion(self, pos, curr, low_tight, high_tight):
+        if pos == len(self.high):
+            #print(curr)
+            self.answers.append(int(curr))
+            return
+        # when you already reach 9 before the last digit
+        if curr and curr[-1] == "9":
+            return
+        start, end = 0, 9
+        if low_tight:
+            start = int(self.low[pos])
+        if high_tight:
+            end = int(self.high[pos])
+        prev_digit = int(curr[-1]) if len(curr) > 0 else None
+        if prev_digit is None:
+            for digit in range(start, end + 1):
+                next_low_tight = False
+                next_high_tight = False
+                if low_tight and digit == start:
+                    next_low_tight = True
+                if high_tight and digit == end:
+                    next_high_tight = True
+                self.recursion(pos + 1, str(digit), next_low_tight, next_high_tight)
+        else:
+            leading_zeros = len(curr) if curr and curr[-1] == "0" else 0
+            if leading_zeros == 0:
+                digit = prev_digit + 1
+                if digit < start or digit > end:
+                    return
+                next_low_tight = True if low_tight and digit == start else False
+                next_high_tight = True if high_tight and digit == end else False
+                self.recursion(pos + 1, curr + str(digit), next_low_tight, next_high_tight)
+            else:
+                if self.gap > 0 and leading_zeros == self.gap:
+                    # you want to make it low_tight
+                    #print("Position:", pos)
+                    low_tight = True
+                    start = int(self.low[pos])
 
-        #print(start_digit)
-        length =  len(str_low)-1  # len(str_high) - (len(str_high) - len(str_low)+1)
-        answers = []
-        for start_digit in start_digits:
-            start_num = int(str(start_digit)[-1])
-            if length + start_num <= 9:
-               answer = str(start_digit) + "".join([str(i + start_num) for i in range(1, length+1)])
-               answer = int(answer)
-               #print(answer)
-               if low <= answer and answer <= high:
-                answers.append(answer)
-        return answers 
+                    # you also want to start iteration
+                for digit in range(start, end + 1):
+                    next_low_tight = False
+                    next_high_tight = False
+                    if low_tight and digit == start:
+                        next_low_tight = True
+                    if high_tight and digit == end:
+                        next_high_tight = True
+                    self.recursion(pos + 1, curr + str(digit), next_low_tight, next_high_tight)
+            
+            
+
+
+        
+        
 
         
