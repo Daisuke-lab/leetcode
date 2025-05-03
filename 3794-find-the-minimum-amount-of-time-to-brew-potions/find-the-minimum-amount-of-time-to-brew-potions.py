1ec8408f3acde4cbba86bafa295042fb3e02960a
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-03 15:53:59 -0500

    Time: 13568 ms (26.34%) | Memory: 18.8 MB (54.30%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.py
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.py
@@ -1,19 +1,38 @@
 class Solution:
-    def minTime(self, skill: List[int], mana: List[int]) -> int:
-        n, m = len(skill), len(mana)
-        
-        sPre = [0] * n
-        for i in range(1, n):
-            sPre[i] = sPre[i - 1] + skill[i]
+    # define start_time
+    # create prefix sum
+    # compare the prev result vs prefix + start_time
+    # take the biggest gap and set it as start_time
+    def minTime(self, skills: List[int], manas: List[int]) -> int:
+        start_time = 0
+        prefix_sums = self.get_prefix_sums(skills)
+        print(prefix_sums)
+        for i, mana in enumerate(manas):
+            prev_start_time = start_time
+            start_time += manas[i-1] * skills[0] if i != 0 else 0 
+            max_delay = 0
+            for j, prefix_sum in enumerate(prefix_sums):
+                prev_task_finish_time = prev_start_time + prefix_sum * (manas[i-1] if i!= 0 else 0)
+                prev_wizard_estimate_finish_time = start_time + (prefix_sums[j-1] * manas[i] if j != 0 else 0)
+                delay = prev_task_finish_time - prev_wizard_estimate_finish_time
+                # print("Previous task finshed at:                   ", prev_task_finish_time)
+                # print("Previous wizard will finish current task at:", prev_wizard_estimate_finish_time)
+                max_delay = max(max_delay, delay)
+            start_time += max_delay
+            #print("-----------------------------------------------")
+            #print(start_time)
+        return start_time + manas[-1] * prefix_sums[-1]
 
-        tSum = skill[0] * mana[0]
+    def get_prefix_sums(self, skills):
+        prefix_sums = []
+        for skill in skills:
+            if len(prefix_sums) == 0:
+                prefix_sums.append(skill)
+            else:
+                prefix_sums.append(skill + prefix_sums[-1])
+        return prefix_sums
+            
 
-        for j in range(1, m):
-            tMax = skill[0] * mana[j]
-            for i in range(1, n):
-                tDiff = sPre[i] * mana[j - 1] - sPre[i - 1] * mana[j]
-                if tDiff > tMax:
-                    tMax = tDiff
-            tSum += tMax
 
-        return tSum + sPre[n - 1] * mana[m - 1]
+
+            
