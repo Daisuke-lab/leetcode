34a74762e8407f7e641df884cc3c6a89abd2d66d
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-08 13:45:45 -0500

    Time: 6 ms (95.30%) | Memory: 56.6 MB (96.41%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,69 @@
+class Solution {
+    int[] nums;
+    public int findKthLargest(int[] nums, int k) {
+        this.nums = nums;
+        return quickSelect(0, nums.length -1, nums.length - k+ 1);
+    }
+
+    public int quickSelect(int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
+        }
+        // System.out.println("--------------------------");
+        // System.out.println(left);
+        // System.out.println(right);
+        // System.out.println(k);
+        int pivotIndex = getPivot(left, right);
+        int pivotNum = nums[pivotIndex];
+        nums[pivotIndex] = nums[right];
+        nums[right] = pivotNum;
+        int i = left;
+        int j = right - 1;
+        while (true) {
+            while (i <= j && nums[i] < pivotNum) {
+                i++;
+            }
+            while (i <= j && nums[j] > pivotNum) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            } else {
+                break;
+            }
+        }
+        nums[right] = nums[i];
+        nums[i] = pivotNum;
+        // System.out.println("pivot Num:" + Integer.toString(pivotNum));
+        // System.out.println("i:" + Integer.toString(i));
+        if (i == k -1) {
+            return pivotNum;
+        } else if (i < k - 1) {
+            return quickSelect(i+1, right, k);
+        } else {
+            return quickSelect(left, i-1, k);
+        }
+        
+        
+
+    }
+
+    public int getPivot(int min, int max) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        List<Integer> numList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            int randomIndex = random.nextInt(max - min + 1) + min;
+            numMap.put(nums[randomIndex], randomIndex);
+            numList.add(nums[randomIndex]);
+        }
+        Collections.sort(numList);
+        int middleNum = numList.get(1);
+        return numMap.get(middleNum);
+        
+    }
+}
