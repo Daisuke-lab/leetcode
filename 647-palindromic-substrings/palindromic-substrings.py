bb895fd5d0ab9eaa299f81a5a172d31bda873ad0
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-12 16:17:10 -0500

    Time: 2 ms (99.68%) | Memory: 17.8 MB (51.49%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,30 +1,24 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-
-        def manacher(s):
-            t = '^#' + '#'.join(s) + '#$'
-            n = len(t)
-            pali_radiuses = [0] * n
-            center, right = 0, 0
-            for i in range(n):
-                radius = 0
-                if i < right:
-                    distance = i - center
-                    mirror = center - distance
-                    radius = min(right - i, pali_radiuses[mirror])
-                l = i - radius - 1
-                r = i + radius + 1
-                while l >= 0 and r < n and t[l] == t[r]:
-                    l -= 1
-                    r += 1
-                    radius += 1
-                if i + radius > right:
-                    center, right = i, i + radius
-                pali_radiuses[i] = radius
-            return pali_radiuses
-        
-        p = manacher(s)
-        res = 0
-        for i in p:
-            res += (i + 1) // 2
-        return res
+        t = '^#' + '#'.join(s) + '#$'
+        n = len(t)
+        pali_radiuses = [0] * n
+        center, right = 0, 0
+        count = 0
+        for i in range(n):
+            radius = 0
+            if i < right:
+                distance = i - center
+                mirror = center - distance
+                radius = min(right - i, pali_radiuses[mirror])
+            l = i - radius - 1
+            r = i + radius + 1
+            while l >= 0 and r < n and t[l] == t[r]:
+                l -= 1
+                r += 1
+                radius += 1
+            if i + radius > right:
+                center, right = i, i + radius
+            pali_radiuses[i] = radius
+            count += (radius + 1) // 2
+        return count
