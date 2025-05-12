4b496589457569a9d40cf20c070fb9c1f4aaa75b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-12 16:10:19 -0500

    Time: 439 ms (13.48%) | Memory: 17.8 MB (51.49%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -5,13 +5,16 @@ class Solution:
             t = '^#' + '#'.join(s) + '#$'
             n = len(t)
             pali_radiuses = [0] * n
-            l, r = 0, 0
+            left, right = 0, 0
             for i in range(n):
-                pali_radiuses[i] = min(r - i, pali_radiuses[l + (r - i)]) if i < r else 0
+                if i < right:
+                    pali_radiuses[i] = min(r - i, pali_radiuses[l + (r - i)])
+                else:
+                    pali_radiuses[i] = 0
                 while (i + pali_radiuses[i] + 1 < n and i - pali_radiuses[i] - 1 >= 0 
                        and t[i + pali_radiuses[i] + 1] == t[i - pali_radiuses[i] - 1]):
                     pali_radiuses[i] += 1
-                if i + pali_radiuses[i] > r:
+                if i + pali_radiuses[i] > right:
                     l, r = i - pali_radiuses[i], i + pali_radiuses[i]
             return pali_radiuses
         
