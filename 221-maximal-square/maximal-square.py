a40e26eef9c6750907fc01dbf2882971e658e843
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-12 13:45:53 -0500

    Time: 165 ms (36.56%) | Memory: 33.7 MB (33.78%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -3,11 +3,27 @@ class Solution:
         ROW = len(matrix)
         COL = len(matrix[0])
         
-        grid = [[0 for j in range(COL+1)] for i in range(ROW + 1)]
+        grid = [[0 for j in range(COL)] for i in range(ROW)]
         max_length = 0
-        for i in range(1, ROW + 1):
-            for j in range(1, COL + 1):
-                if matrix[i-1][j-1] == '1':
-                    grid[i][j] = min(grid[i-1][j], grid[i][j-1], grid[i-1][j-1]) + 1
+        for i in range(ROW):
+            for j in range(COL):
+                if i == 0 or j == 0:
+                    grid[i][j] = int(matrix[i][j])
                     max_length = max(max_length, grid[i][j])
+                    continue
+                if matrix[i][j] == '1':
+                    ensured_length = min(grid[i-1][j], grid[i][j-1])
+                    possible_length = ensured_length + 1
+                    # if possible_length == 3:
+                    #     print(i, j)
+                    dig_i = i - (possible_length - 1)
+                    dig_j = j - (possible_length - 1)
+                    if matrix[dig_i][dig_j] == "1":
+                        grid[i][j] = possible_length
+                    elif ensured_length > 0:
+                        grid[i][j] = ensured_length
+                    else:
+                        grid[i][j] = 1
+                    max_length = max(max_length, grid[i][j])
+        print(grid)
         return max_length**2
