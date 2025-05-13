6532cf76f49a6ef6f25f4b651d22836141d57947
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-13 17:28:04 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (10.35%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -4,13 +4,17 @@ class Solution:
 
         i, j = 0, 0
         while i < len(haystack):
+            # when chars match
             if haystack[i] == needle[j]:
                 i += 1
                 j += 1
-            elif j == 0:
-                i += 1
-            else:
+            # don't start from j=0, we can utilize lps
+            elif j != 0:
                 j = lps[j-1]
+            # when j=0, nothing can match with i, so move on
+            else:
+                i += 1
+                
 
             if j == len(needle):
                 return i - j
@@ -21,14 +25,18 @@ class Solution:
         lps = [0] * len(word)
         prev_lps, i = 0, 1
         while i < len(word):
+            # when you can utilize the previous suffix/prefix
             if word[i] == word[prev_lps]:
                 lps[i] = prev_lps + 1
                 prev_lps += 1
                 i += 1
-            elif prev_lps == 0:
+            # if the first condition doesn't work, you go back and search suffix/prefix you can use
+            elif prev_lps != 0:
+                prev_lps = lps[prev_lps-1]
+            # when there is not suffix/prefix you can use
+            else:
                 lps[i] = 0
                 i += 1
-            else:
-                prev_lps = lps[prev_lps-1]
+                
         return lps
         
