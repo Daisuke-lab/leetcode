9e730b3a591544405b247f587afa291e8ae71a3f
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-14 14:30:06 -0500

    Time: 16 ms (7.04%) | Memory: 46.2 MB (40.84%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,18 +1,24 @@
 class Solution {
-    private Map<Integer, List<Integer>> adList;
-    private List<Integer> answer;
+    private Map<Integer, List<Integer>> outList;
+    private Map<Integer, List<Integer>> inList;
+    private Stack<Integer> zeroIncomings;
     private Set<Integer> visited;
+    private List<Integer> answer;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        adList = initAdList(numCourses, prerequisites);
+        outList = initOutList(numCourses, prerequisites);
+        inList = initInList(numCourses, prerequisites);
+        zeroIncomings = initStack();
         visited = new HashSet<>();
         answer = new ArrayList<>();
-        for (int i=0; i < numCourses; i++) {
-            if (!visited.contains(i)) {
-                if (findCycle(i, new HashSet<>())) {
+        while (!zeroIncomings.empty()) {
+            int i = zeroIncomings.pop();
+            if (findCycle(i, new HashSet<>())) {
                     return new int[0];
                 }
-            }
         }
+        if (answer.size() != numCourses) {
+            return new int[0];
+    }
         Collections.reverse(answer);
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
@@ -23,12 +29,11 @@ class Solution {
             return true;
         }
         path.add(curr);
-        for (int ad: adList.get(curr)) {
+        for (int ad: outList.get(curr)) {
             if (findCycle(ad, path)) {
                 return true;
             }
         }
-        adList.put(curr, new ArrayList<>());
         if (!visited.contains(curr)) {
             answer.add(curr);
         }
@@ -38,7 +43,29 @@ class Solution {
 
     }
 
-    public Map<Integer, List<Integer>> initAdList(int n, int[][] edges) {
+    public Stack<Integer> initStack() {
+        Stack<Integer> stack = new Stack<>();
+        for (int key: inList.keySet()) {
+            if (inList.get(key).size() == 0) {
+                stack.add(key);
+            }
+        }
+        return stack;
+    }
+
+    public Map<Integer, List<Integer>> initInList(int n, int[][] edges) {
+        Map<Integer, List<Integer>> adList = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            adList.put(i, new ArrayList<>());
+        }
+        for (int[] edge: edges) {
+            adList.get(edge[0]).add(edge[1]);
+        }
+        return adList;
+    }
+
+
+    public Map<Integer, List<Integer>> initOutList(int n, int[][] edges) {
         Map<Integer, List<Integer>> adList = new HashMap<>();
         for (int i = 0; i < n; i++) {
             adList.put(i, new ArrayList<>());
