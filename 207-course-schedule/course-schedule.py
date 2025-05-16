2f4e492eb9c8e312a7aed589878ad07c170867a2
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-16 16:53:19 -0500

    Time: 989 ms (5.02%) | Memory: 19.3 MB (50.23%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,17 +1,12 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         self.out_list = self.init_out_list(numCourses, prerequisites)
-        self.in_list = self.init_in_list(numCourses, prerequisites)
-        queue = self.collect_zero_incoming_vertices(self.in_list)
         self.topological_sort = []
         self.visited = set()
-        while queue:
-            node = queue.popleft()
+        for node in range(numCourses):
             if node in self.visited:
                 continue
-            if self.dfs(node, set()) is False:
-                return False
-        #print(self.topological_sort)
+            self.dfs(node, set())
         return len(self.topological_sort) == numCourses
 
     def dfs(self, node, path):
@@ -24,6 +19,7 @@ class Solution:
             if self.dfs(ad, path) is False:
                 return False
         path.remove(node)
+        self.out_list[node] = set()
         self.topological_sort.append(node)
         self.visited.add(node)
         return True
@@ -36,17 +32,4 @@ class Solution:
             out_list[node1].add(node2)
         return out_list
 
-    def init_in_list(self, n, edges):
-        in_list = {i: set() for i in range(n)}
-        for node1, node2 in edges:
-            in_list[node2].add(node1)
-        return in_list
-
-    def collect_zero_incoming_vertices(self, in_list):
-        queue = collections.deque()
-        for node in in_list.keys():
-            if len(in_list[node]) == 0:
-                queue.append(node)
-
-        return queue
         
