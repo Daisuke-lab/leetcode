f318691dea2602c4ffc5a6c8e8ac4e28949f3160
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-16 17:11:56 -0500

    Time: 9 ms (19.38%) | Memory: 19.3 MB (40.02%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,35 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        self.ad_list = self.init_ad_list(prerequisites)
-        #print(self.ad_list)
-        self.visited = set()
-        for v in self.ad_list.keys():
-            if v not in self.visited:
-                has_cycle = self.has_cycle(v, set())
-                if has_cycle:
-                    return False
-        return True
+        self.out_list = self.init_out_list(numCourses, prerequisites)
+        self.topological_sort = []
+        for node in range(numCourses):
+            if node not in self.out_list:
+                continue
+            if self.dfs(node, set()) is False:
+                return False
+        #print(self.topological_sort)
+        return len(self.topological_sort) == numCourses
 
-
-    def has_cycle(self, start, visited):
-        self.visited.add(start)
-        if start in visited:
+    def dfs(self, node, path):
+        if node in path:
+            return False
+        if node not in self.out_list:
             return True
-        visited.add(start)
-        for v in self.ad_list[start]:
-            has_cycle = self.has_cycle(v, visited)
-            if has_cycle:
-                visited.remove(start)
-                return True
-        self.ad_list[start] = []
-        visited.remove(start)
-        return False
+        path.add(node)
+        for ad in self.out_list[node]:
+            if self.dfs(ad, path) is False:
+                return False
+        path.remove(node)
+        del self.out_list[node]
+        self.topological_sort.append(node)
+        return True
+        
 
-    def init_ad_list(self, edges):
-        ad_list = {}
-        for v1, v2 in edges:
-            v1_list = ad_list.get(v1, [])
-            v1_list.append(v2)
-            ad_list[v1] = v1_list
-            ad_list[v2] = ad_list.get(v2, []) 
-        return ad_list
+
+    def init_out_list(self, n, edges):
+        out_list = {i: set() for i in range(n)}
+        for node1, node2 in edges:
+            out_list[node1].add(node2)
+        return out_list
