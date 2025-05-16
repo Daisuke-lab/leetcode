1d59f1b50a84db5b528dc6d2d7785e3c630e48d6
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-16 16:56:46 -0500

    Time: 11 ms (15.02%) | Memory: 19.2 MB (50.23%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,35 +1,35 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        self.out_list = self.init_out_list(numCourses, prerequisites)
-        self.topological_sort = []
+        self.ad_list = self.init_ad_list(prerequisites)
+        #print(self.ad_list)
         self.visited = set()
-        for node in range(numCourses):
-            if node in self.visited:
-                continue
-            self.dfs(node, set())
-        return len(self.topological_sort) == numCourses
-
-    def dfs(self, node, path):
-        if node in path:
-            return False
-        if node in self.visited:
-            return True
-        path.add(node)
-        for ad in self.out_list[node]:
-            if self.dfs(ad, path) is False:
-                return False
-        path.remove(node)
-        self.out_list[node] = set()
-        self.topological_sort.append(node)
-        self.visited.add(node)
+        for v in self.ad_list.keys():
+            if v not in self.visited:
+                has_cycle = self.has_cycle(v, set())
+                if has_cycle:
+                    return False
         return True
-        
 
 
-    def init_out_list(self, n, edges):
-        out_list = {i: set() for i in range(n)}
-        for node1, node2 in edges:
-            out_list[node1].add(node2)
-        return out_list
+    def has_cycle(self, start, visited):
+        self.visited.add(start)
+        if start in visited:
+            return True
+        visited.add(start)
+        for v in self.ad_list[start]:
+            has_cycle = self.has_cycle(v, visited)
+            if has_cycle:
+                visited.remove(start)
+                return True
+        self.ad_list[start] = []
+        visited.remove(start)
+        return False
 
-        
+    def init_ad_list(self, edges):
+        ad_list = {}
+        for v1, v2 in edges:
+            v1_list = ad_list.get(v1, [])
+            v1_list.append(v2)
+            ad_list[v1] = v1_list
+            ad_list[v2] = ad_list.get(v2, []) 
+        return ad_list
