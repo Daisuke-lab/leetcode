d4065857f8d0fe25bb77d685f46fc37dbcabe021
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-16 19:18:33 -0500

    Time: 68 ms (34.29%) | Memory: 32.7 MB (7.03%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,31 +1,43 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if len(edges) == 0:
+        if n == 1:
             return [0]
-        ad_list = self.init_ad_list(n, edges)
-        leaves = self.get_leaves(ad_list)
-        while leaves:
-            if n <= 2:
-                return list(leaves)
-            for i in range(len(leaves)):
-                n -= 1
-                leaf = leaves.popleft()
-                for ad in ad_list[leaf]:
-                    ad_list[ad].remove(leaf)
-                    if len(ad_list[ad]) == 1:
-                        leaves.append(ad)
-        
-        
 
-    def get_leaves(self, ad_list):
-        leaves = collections.deque()
-        for node in ad_list.keys():
-            if len(ad_list[node]) == 1:
-                leaves.append(node)
-        return leaves
-    def init_ad_list(self, n, edges):
-        ad_list = {i: set() for i in range(n)}
-        for v1, v2 in edges:
-            ad_list[v1].add(v2)
-            ad_list[v2].add(v1)
-        return ad_list
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def dfs(node, parent):
+            farthest_node = node
+            max_distance = 0
+            for nei in adj[node]:
+                if nei != parent:
+                    nei_node, nei_distance = dfs(nei, node)
+                    if nei_distance + 1 > max_distance:
+                        max_distance = nei_distance + 1
+                        farthest_node = nei_node
+            return farthest_node, max_distance
+
+        node_a, _ = dfs(0, -1)
+        node_b, diameter = dfs(node_a, -1)
+
+        centroids = []
+
+        def find_centroids(node, parent):
+            if node == node_b:
+                centroids.append(node)
+                return True
+            for nei in adj[node]:
+                if nei != parent:
+                    if find_centroids(nei, node):
+                        centroids.append(node)
+                        return True
+            return False
+
+        find_centroids(node_a, -1)
+        L = len(centroids)
+        if diameter % 2 == 0:
+            return [centroids[L // 2]]
+        else:
+            return [centroids[L // 2 - 1], centroids[L // 2]]
