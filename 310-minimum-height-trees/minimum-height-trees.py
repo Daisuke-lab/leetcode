a64a6689bd36db404995f35c05358550244ff9ad
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-16 19:42:51 -0500

    Time: 83 ms (20.96%) | Memory: 33.7 MB (5.69%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -2,43 +2,50 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
+        self.ad_list = self.init_ad_list(n, edges)
+        edge_node,_ = self.get_farest_node(0, None)
+        edge_node2,_ = self.get_farest_node(edge_node, None)
+        #print(edge_node, edge_node2)
+        path = self.collect_path(edge_node, edge_node2)
+        #print(path)
 
-        adj = [[] for _ in range(n)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-
-        def dfs(node, parent):
-            farthest_node = node
-            max_distance = 0
-            for nei in adj[node]:
-                if nei != parent:
-                    nei_node, nei_distance = dfs(nei, node)
-                    if nei_distance + 1 > max_distance:
-                        max_distance = nei_distance + 1
-                        farthest_node = nei_node
-            return farthest_node, max_distance
-
-        node_a, _ = dfs(0, -1)
-        node_b, diameter = dfs(node_a, -1)
-
-        centroids = []
-
-        def find_centroids(node, parent):
-            if node == node_b:
-                centroids.append(node)
-                return True
-            for nei in adj[node]:
-                if nei != parent:
-                    if find_centroids(nei, node):
-                        centroids.append(node)
-                        return True
-            return False
-
-        find_centroids(node_a, -1)
-        L = len(centroids)
-        print(centroids)
-        if len(centroids) % 2 == 1:
-            return [centroids[L // 2]]
+        #print(centroids)
+        m = len(path) // 2
+        if len(path) % 2 == 1:
+            return [path[m]]
         else:
-            return [centroids[L // 2 - 1], centroids[L // 2]]
+            return [path[m - 1], path[m]]
+
+    def init_ad_list(self, n, edges):
+        ad_list = {i: set() for i in range(n)}
+        for v1, v2 in edges:
+            ad_list[v1].add(v2)
+            ad_list[v2].add(v1)
+        return ad_list
+        
+    def get_farest_node(self, node, parent):
+        farthest_node = node
+        max_distance = 0
+        for ad in self.ad_list[node]:
+            if ad != parent:
+                far_node, distance = self.get_farest_node(ad, node)
+                if distance + 1 > max_distance:
+                    max_distance = distance + 1
+                    farthest_node = far_node
+        return farthest_node, max_distance
+
+    def collect_path(self, v1, v2, parent=None):
+        #print(v1, v2)
+        if v1 == v2:
+            return [v2]
+        for ad in self.ad_list[v1]:
+            if ad == parent:
+                continue
+            path = self.collect_path(ad, v2, v1)
+            if len(path) != 0:
+                path.append(v1)
+                return path
+        return []
+        
+
+        
