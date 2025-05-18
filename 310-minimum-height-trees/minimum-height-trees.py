b6b27caaf5092b0ea65ec42015f0bbc49b10a88e
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-17 19:56:15 -0500

    Time: 115 ms (12.52%) | Memory: 36.4 MB (5.05%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,20 +1,44 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n == 1:
-            return [0]
         self.ad_list = self.init_ad_list(n, edges)
-        edge_node,_ = self.get_farest_node(0)
-        edge_node2,_ = self.get_farest_node(edge_node)
-        #print(edge_node, edge_node2)
-        path = self.collect_path(edge_node, edge_node2)
-        #print(path)
+        # self.memo[i][0]: tallest subtree height from node i (max height from children)
+        # self.memo[i][1]: second tallest subtree height from node i
+        self.memo = [[0] * 2 for _ in range(n)]
+        self.build_heights_to(0)
+        self.build_heights_from(0)
+        min_height = min([self.memo[i][0] for i in range(n)])
+        answer = [i for i in range(n) if self.memo[i][0] == min_height]
+        return answer
+        
+    def build_heights_to(self, node, parent=None):
+        for ad in self.ad_list[node]:
+            if ad == parent:
+                continue
+            height = self.build_heights_to(ad, node) + 1
+            if height > self.memo[node][0]:
+                self.memo[node][1] = self.memo[node][0]
+                self.memo[node][0] = height
+            elif height > self.memo[node][1]:
+                self.memo[node][1] = height
+        return self.memo[node][0]
 
-        #print(centroids)
-        m = len(path) // 2
-        if len(path) % 2 == 1:
-            return [path[m]]
-        else:
-            return [path[m - 1], path[m]]
+
+    def build_heights_from(self, node, parent=None, curr_height=0):
+        if curr_height > self.memo[node][0]:
+            self.memo[node][1] = self.memo[node][0]
+            self.memo[node][0] = curr_height
+        elif curr_height > self.memo[node][1]:
+            self.memo[node][1] = curr_height
+
+        for ad in self.ad_list[node]:
+            if ad == parent:
+                continue
+            if self.memo[node][0] == 1 + self.memo[ad][0]:
+                next_height = 1 + self.memo[node][1]
+            else:
+                next_height = 1 + self.memo[node][0]
+            self.build_heights_from(ad, node, next_height)
+
 
     def init_ad_list(self, n, edges):
         ad_list = {i: set() for i in range(n)}
@@ -22,30 +46,6 @@ class Solution:
             ad_list[v1].add(v2)
             ad_list[v2].add(v1)
         return ad_list
-        
-    def get_farest_node(self, node, parent=None):
-        farthest_node = node
-        max_distance = 0
-        for ad in self.ad_list[node]:
-            if ad != parent:
-                far_node, distance = self.get_farest_node(ad, node)
-                if distance + 1 > max_distance:
-                    max_distance = distance + 1
-                    farthest_node = far_node
-        return farthest_node, max_distance
-
-    def collect_path(self, v1, v2, parent=None):
-        #print(v1, v2)
-        if v1 == v2:
-            return [v2]
-        for ad in self.ad_list[v1]:
-            if ad == parent:
-                continue
-            path = self.collect_path(ad, v2, v1)
-            if len(path) != 0:
-                path.append(v1)
-                return path
-        return []
         
 
         
