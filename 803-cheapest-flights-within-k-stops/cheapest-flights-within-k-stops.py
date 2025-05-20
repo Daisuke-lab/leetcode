baa75bc07f0f3938ea636b7426db281f1a2adc9b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-20 17:14:34 -0500

    Time: 39 ms (39.60%) | Memory: 19.2 MB (46.03%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,31 +1,22 @@
 class Solution:
-    # 
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        ad_list = self.init_ad_list(n, flights)
-        dj_map = [float("inf") for i in range(n)]
-        dj_map[src] = 0
-        for i in range(k+1):
-            new_dj_map = dj_map.copy()
+        self.ad_list = self.init_ad_list(n, flights)
+        bf_map = [float("inf") for i in range(n)]
+        bf_map[src] = 0
+        for _k in range(k + 1):
+            new_bf_map = bf_map.copy()
             for node in range(n):
-                if dj_map[node] == float("inf"):
+                # when you haven't arrived node in _k time yet
+                if bf_map[node] == float("inf"):
                     continue
-                spent = dj_map[node]
-                for ad, price in ad_list[node]:
-                    if new_dj_map[ad] > spent + price:
-                        new_dj_map[ad] = spent + price
-            dj_map = new_dj_map
-            #print(dj_map)           
-        
-        
-        return dj_map[dst] if dj_map[dst] != float("inf") else - 1
-
-        
-
-        
-        
+                for price, ad in self.ad_list[node]:
+                    if new_bf_map[ad] > bf_map[node] + price:
+                        new_bf_map[ad] = bf_map[node] + price
+            bf_map = new_bf_map
+        return bf_map[dst] if bf_map[dst] != float("inf") else -1
 
     def init_ad_list(self, n, flights):
         ad_list = [set() for i in range(n)]
         for src, dest, price in flights:
-            ad_list[src].add((dest, price))
+            ad_list[src].add((price, dest))
         return ad_list      
