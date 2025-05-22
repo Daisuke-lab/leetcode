b58dd8b37785409e2a1fdc7ab3a320459bc58745
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-21 19:43:53 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (73.34%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,6 +1,7 @@
 class UnionFind:
     def __init__(self):
         self.parent = {}
+        # you want to put actual value to self.weight like a = 2.0 or b = 3.0 (not division)
         self.weight = {}
     
     def add(self, x):
@@ -12,6 +13,9 @@ class UnionFind:
         if x != self.parent[x]:
             orig_parent = self.parent[x]
             self.parent[x] = self.find(self.parent[x])
+            # bigger value (e.g. a) is a child and smaller value at the root
+            # So you are going to multiply by parent weight
+            # * you have to do this as a last step because all the weight need to be updated from the root before you udpate x
             self.weight[x] *= self.weight[orig_parent]
         return self.parent[x]
     
@@ -20,9 +24,12 @@ class UnionFind:
         self.add(y)
         root_x = self.find(x)
         root_y = self.find(y)
-        
         if root_x != root_y:
             self.parent[root_x] = root_y
+            # 1. a/b=2 => a = 2, b = 1, but "b" is the root!
+            # 2. b/c=3 => b=3, c = 1
+            # => next time you find(), you have to update a to 6, so that a/b is still true
+            # 
             self.weight[root_x] = value * self.weight[y] / self.weight[x]
     
     def get_ratio(self, x, y):
@@ -36,6 +43,8 @@ class Solution:
         
         for (a, b), value in zip(equations, values):
             uf.union(a, b, value)
-        print(uf.weight)
+        #print(uf.weight)
         
-        return [uf.get_ratio(a, b) for a, b in queries]
+        result = [uf.get_ratio(a, b) for a, b in queries]
+        #print(uf.weight)
+        return result
