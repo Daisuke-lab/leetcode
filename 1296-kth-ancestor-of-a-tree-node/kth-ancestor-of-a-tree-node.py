531d1eb2760d3b8caf3dc16bdfa9f51b04865afb
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-22 20:07:42 -0500

    Time: 350 ms (45.22%) | Memory: 54.2 MB (23.16%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.py
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.py
@@ -1,38 +1,49 @@
 class TreeAncestor:
 
     def __init__(self, n: int, parent: List[int]):
-        self.lift_height = ceil(math.log(n, 2)) + 1
-        # anceestor[node][exponent] = 2^exponent parent
+        self.max_exponent = ceil(math.log(n, 2)) + 1
         self.ancestors = [[
-            -1 for j in range(self.lift_height)]
-            for i in range(n)]
-            
-        # 2^0 = 1, insert your parent with O(1) time
+            -1 for i in range(self.max_exponent)]
+            for j in range(n)]
+        self.parents = parent
+
         for node in range(n):
-            self.ancestors[node][0] = parent[node]
-        # any ancestor of root is defined as -1
-        for jump in range(self.lift_height):
-            self.ancestors[0][jump] = -1
-        for jump in range(1, self.lift_height):
+            self.ancestors[node][0] = self.parents[node]
+        for exponent in range(self.max_exponent):
+            self.ancestors[0][exponent] = -1
+
+        for exponent in range(1, self.max_exponent):
             for node in range(1, n):
-                prev = self.ancestors[node][jump - 1]
+                # the 2^n-1 parent of node
+                # the 2^n-1 parent of the 2^n-1 parent
+                # (2^(x-1)) + (2^(x-1)) = (2^(x))
+                prev = self.ancestors[node][exponent -1]
                 if prev != -1:
-                    # 2^jump-1 parent of 2^jump-1 parent is 2^jump parent
-                    #(2^(x-1)) + (2^(x-1)) = (2^(x))
-                    self.ancestors[node][jump] = self.ancestors[prev][jump - 1]
-        #print(self.ancestors)
+                    curr = self.ancestors[prev][exponent -1]
+                    self.ancestors[node][exponent] = curr
+
 
 
     def getKthAncestor(self, node: int, k: int) -> int:
-        for i in range(self.lift_height):
+        # collect exponents
+        # k = 2^n + 2^m + 2^s + .....
+        # e.g. k = 6 = 4 + 2 => 
+        # 100
+        for i in range(self.max_exponent):
             # if k = 2^i + p
             if k & (1 << i): 
                 node = self.ancestors[node][i]
                 if node == -1:
                     return -1  # No such ancestor
         return node
-        
 
+        binary = bin(k)[2:]
+        curr = node
+        for i, bit in enumerate(binary):
+            if bit == "1":
+                exponent = len(binary) - i -1
+                curr = self.ancestors[curr][exponent]
+        return curr
 
 # Your TreeAncestor object will be instantiated and called as such:
 # obj = TreeAncestor(n, parent)
