63c99668c2f76f75b0302d2bee57f1a92dd9f19d
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-27 19:10:23 -0500

    Time: 83 ms (45.62%) | Memory: 33.1 MB (7.13%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,23 +1,6 @@
 class Solution:
-    # you want to start from positive number for sure
-    # you want to finish with positive number for sure
-    # you have two options
-    # case1: include current as subarray
-    # case2: stop sub array
-    # DP
-    #
     def maxSubArray(self, nums: List[int]) -> int:
-        tab = [0, 0]
-        max_point = nums[0]
-        for i in range(len(nums)):
-            if i == 0:
-                tab[1] = nums[i]
-            else:
-                if tab[1] > 0:
-                    tab_num = tab[1] + nums[i]
-                else:
-                    tab_num = nums[i]
-                tab[0] = tab[1]
-                tab[1] = tab_num
-                max_point = max(tab[1], max_point)
-        return max_point
+        tab = [num for num in nums]
+        for i in range(1, len(nums)):
+            tab[i] = max(tab[i-1] + nums[i], nums[i])
+        return max(tab)
