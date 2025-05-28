e7d9ea9640a31d6fc316eca23da8f57301a17332
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-05-27 19:33:10 -0500

    Time: 149 ms (44.09%) | Memory: 32.7 MB (90.08%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -2,25 +2,22 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         ROW = len(matrix)
         COL = len(matrix[0])
-        
-        grid = [[0 for j in range(COL)] for i in range(ROW)]
+        memo = [[
+            0 for j in range(COL)]
+            for i in range(ROW)]
         max_length = 0
         for i in range(ROW):
             for j in range(COL):
-                if i == 0 or j == 0:
-                    grid[i][j] = int(matrix[i][j])
-                    max_length = max(max_length, grid[i][j])
+                if matrix[i][j] == "0":
                     continue
-                if matrix[i][j] == '1':
-                    ensured_length = min(grid[i-1][j], grid[i][j-1])
-                    possible_length = ensured_length + 1
-                    dig_i = i - (possible_length - 1)
-                    dig_j = j - (possible_length - 1)
-                    if matrix[dig_i][dig_j] == "1":
-                        grid[i][j] = possible_length
-                    elif ensured_length > 0:
-                        grid[i][j] = ensured_length
+                elif i == 0 or j == 0:
+                    memo[i][j] = int(matrix[i][j])
+                else:
+                    ensured_length = min(memo[i-1][j], memo[i][j-1])
+                    potential_length = ensured_length + 1
+                    if matrix[i-(potential_length-1)][j-(potential_length-1)] == "1":
+                        memo[i][j] = potential_length
                     else:
-                        grid[i][j] = 1
-                    max_length = max(max_length, grid[i][j])
-        return max_length**2
+                        memo[i][j] = max(1, ensured_length)
+                max_length = max(max_length, memo[i][j])
+        return max_length * max_length
