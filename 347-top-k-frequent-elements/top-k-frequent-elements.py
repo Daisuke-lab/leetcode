53ff597e3e684155856ae25fcfc08909595beb03
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-03 16:55:55 -0500

    Time: 27 ms (5.21%) | Memory: 32.3 MB (7.74%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,23 +1,45 @@
 class Solution:
-    # Hashmap: n + nlogn
-    # min Heap with size k 3n
-    # 
+    # Bucket List + hashmap
+    # compress
+    # quick selecct 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_map = defaultdict(int)
+        buckets = [set() for i in range(len(nums) + 1)]
+        count_map = {}
         for num in nums:
-            count_map[num] += 1
+            if num not in count_map:
+                count_map[num] = 1
+                buckets[1].add(num)
+            else:
+                curr_count = count_map[num]
+                buckets[curr_count].remove(num)
+                count_map[num] += 1
+                buckets[curr_count+1].add(num)
+        unique_sorted_nums = []
+        for i in range(len(buckets) -1, -1, -1):
+            bucket = buckets[i]
+            for num in bucket:
+                unique_sorted_nums.append(num)
+            if len(unique_sorted_nums) == k:
+                return  unique_sorted_nums
+
+    def create_unique_nums(self, nums):
+        buckets = [set() for i in range(len(nums) + 1)]
+        count_map = {}
+        for num in nums:
+            if num not in count_map:
+                count_map[num] = 1
+                buckets[1].add(num)
+            else:
+                curr_count = count_map[num]
+                buckets[curr_count].remove(num)
+                count_map[num] += 1
+                buckets[curr_count+1].add(num)
+        unique_sorted_nums = []
+        for i in range(len(buckets) -1, -1, -1):
+            bucket = buckets[i]
+            for num in bucket:
+                unique_sorted_nums.append(num)
+        return  unique_sorted_nums
         
-        max_heap = []
-        for num, count in count_map.items():
-            max_heap.append((-count, num))
-        heapq.heapify(max_heap)
-        answer = []
-        while k > 0:
-            k -= 1
-            _, num = heapq.heappop(max_heap)
-            answer.append(num)
-        return answer
-
-
 
         
