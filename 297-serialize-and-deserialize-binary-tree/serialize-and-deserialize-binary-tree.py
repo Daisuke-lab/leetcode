ec1c08333a2f70d7c3bfcbeed93a87b5936f58bf
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-04 16:07:39 -0500

    Time: 85 ms (62.16%) | Memory: 21.1 MB (79.24%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -12,30 +12,31 @@ class Codec:
         if root is None:
             curr.append("N")
             return curr
-        curr.append(root.val)
-        curr = self.to_list(root.left, curr)
-        curr = self.to_list(root.right, curr)
+        curr.append(str(root.val))
+        self.to_list(root.left, curr)
+        self.to_list(root.right, curr)
         return curr
 
     def serialize(self, root):
-        return str(self.to_list(root,  []))
+        return ",".join(self.to_list(root, []))
 
     def deserialize(self, data):
-        data = data.replace("[", "").replace("]", "").replace("'", "").replace(" ", "")
-        nums = data.split(",")
-        return self.to_tree(nums)
-        
+        print(data)
+        preorder = data.split(",")
+        return self.to_tree(preorder)[0]
 
-    def to_tree(self, nums):
-        val = nums.pop(0)
-        val = int(val) if val != "N" else None
-        if val is None:
-            return None
-        else:
-            root = TreeNode(val)
-            root.left = self.to_tree(nums)
-            root.right = self.to_tree(nums)
-            return root
+    def to_tree(self, nums, i=0):
+        if i >= len(nums):
+            return None, i
+        if nums[i] == "N":
+            i += 1
+            return None, i
+        node = TreeNode(int(nums[i]))
+        i += 1
+        node.left, i = self.to_tree(nums, i)
+        node.right, i = self.to_tree(nums, i)
+
+        return node, i
         #return self.to_tree(data)
 
 
