cb9022b1cecbc91d60a5282d235c63573a723aba
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-03 20:12:48 -0500

    Time: 51 ms (19.53%) | Memory: 19.6 MB (13.34%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.py
+++ 324-wiggle-sort-ii/wiggle-sort-ii.py
@@ -1,83 +1,40 @@
 class Solution:
-    # Good test case: # 13313
-
-    # Brute Force
-    # sort it 
-    # divide into 2
-    # merge it 
-    # O(nlogn)
-
-    # Bucket Sort
-    # sort it num_value as index, count as value
-    # compress it
-    # find the middle point
-    # O(n)
-    # But this is O(n) memory complexity
-    
-    # 2 pointers
-    # greedy
-    # quick select
-    # binary search
-    # 112233
-    # 122133
-    # 213123
-    
-    # find the middle point by quick select
-    # partition i, j => i jumps with 2, j jumps with 1
-    # what is the edge case? 112233, 11333, 11133 => It matters when you have odd numbers
-    # 11313
-    # solution 1: expand from middle => 1,2,3 is a counter example. 2 isn't center
-    # 11223 => 12231
-    # 2 => 121 => 21213
-    # 12132
     def wiggleSort(self, nums: List[int]) -> None:
         if len(nums) == 1:
             return nums
-        #self.quick_select(nums, 0, len(nums) - 1)
-        nums.sort()
+        self.n = len(nums)
+        self.quick_select(nums, 0, len(nums) - 1)
+        #print(nums)
         # if you simply swap it, at max, you have 2 mistakes
         # take it one and concat to the bottom
-        #self.wiggle(nums)
+        self.wiggle(nums)
         #self.adjust(nums)
-        mid = (len(nums) + 1) // 2
-        left, right = nums[:mid][::-1], nums[mid:][::-1]
-        nums[::2], nums[1::2] = left, right
         """
         Do not return anything, modify nums in-place instead.
         """
-
+    def index(self, i):
+            return (1 + 2 * i) % (self.n | 1)
     def wiggle(self, nums):
-        i = 1
-        j = len(nums) -1
-        while i <= j:
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-            i += 2
-            j -= 2
+        i = j = 0
+        k = self.n - 1
+        mid = nums[self.n // 2]
+        #print(mid)
+        while j <= k:
+            if nums[self.index(j)] > mid:
+                nums[self.index(i)], nums[self.index(j)] = nums[self.index(j)], nums[self.index(i)]
+                i += 1
+                j += 1
+            elif nums[self.index(j)] < mid:
+                nums[self.index(j)], nums[self.index(k)] = nums[self.index(k)], nums[self.index(j)]
+                k -= 1
+            else:
+                j += 1
 
-    def adjust(self, nums):
-        invalid = False
-        invalid_num = None
-        direction = "up" if nums[0] < nums[1] else "down"
-        for i in range(len(nums)-1):
-            if invalid:
-                nums[i] = nums[i+1]
-                continue
-            if direction == "up" and nums[i] > nums[i+1]:
-                invalid = True
-                invalid_num = nums[i]
-                nums[i] = nums[i+1]
-            if direction == "down" and nums[i] < nums[i+1]:
-                invalid = True
-                invalid_num = nums[i]
-                nums[i] = nums[i+1]
-            direction = "up" if direction == "down" else "down"
-        if invalid:
-            nums[-1] = invalid_num
                 
 
     def quick_select(self,  nums, i, j):
+        if i >= j:
+            return
         pivot_index = self.get_pivot(nums, i, j)
         pivot_num = nums[pivot_index]
         nums[pivot_index] = nums[j]
@@ -100,7 +57,7 @@ class Solution:
         nums[j] = nums[left]
         nums[left] = pivot_num
 
-        if left == len(nums) // 2:
+        if left== len(nums) // 2:
             return
         if left > len(nums) // 2:
             self.quick_select(nums, i, left-1)
