498c99c5b49f5e7564d61801d56425f3b6298e08
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-04 15:57:10 -0500

    Time: 2 ms (90.89%) | Memory: 18.5 MB (99.83%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,30 +5,50 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # case1: keep going to the left
+    # case2: keep popping from inorder until you get unvisited node
+    # case3: go right once and continue the process
 
+    # variables
+    # i: keep tracking of preorder 
+    # j: keep tracking of inorder
+    # queue: (direction, parent)
+    # visited: set()
+    
+    # base case
+    # queue is not empty (or i and j is inbound)
+    # 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        dummy = TreeNode()
+        i = 0
+        j = 0
         queue = collections.deque()
-        queue.append((dummy, "left"))
+        dummy = TreeNode()
         node_map = {}
-        while preorder:
-            root, direction = queue.pop()
-            val = preorder.pop(0)
-            node = TreeNode(val)
-            node_map[val] = node
+        queue.append((dummy, "left"))
+        visited = set()
+        while queue:
+            parent, direction = queue.popleft()
+            node = TreeNode(preorder[i])
             if direction == "left":
-                root.left = node
-            else:
-                root.right = node
-            if val != inorder[0]:
-                queue.append((node, "left"))
-            else:
-                next_node = node
-                while inorder and inorder[0] in node_map:
-                    next_node = node_map[inorder.pop(0)]
-                queue.append((next_node, "right"))
-        
+                parent.left = node
+            elif direction == "right":
+                parent.right = node
+            i += 1
+            visited.add(node.val)
+            node_map[node.val] = node
+            #queue.append((node, "left"))
+
+            direction = "left"
+            next_parent = node
+            while j < len(inorder) and inorder[j] in visited:
+                next_parent = node_map[inorder[j]]
+                j += 1
+                direction = "right"
+            if i < len(preorder) and j < len(inorder):
+                queue.append((next_parent, direction))
+
         return dummy.left
+                    
 
     
         
