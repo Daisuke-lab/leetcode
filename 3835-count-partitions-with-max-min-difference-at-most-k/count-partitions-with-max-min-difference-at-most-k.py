f3445afaffa1eab25052de7b9de7fe35570c01a9
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-10 16:42:42 -0500

    Time: 551 ms (34.08%) | Memory: 28.9 MB (16.28%) - LeetSync

--- 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.py
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.py
@@ -16,29 +16,37 @@ class Solution:
     # merge: find curr max and min and if it works, + 1
     
     # can you do bottom up
-    def countPartitions(self, A: List[int], k: int) -> int:
-        n = len(A)
-        mod = 10**9 + 7
-        dp = [1] + [0] * n
+    def countPartitions(self, nums: List[int], k: int) -> int:
+        memo = [-1 for i in range(len(nums))]
+        memo[0] = 1
         acc = 1
-        minq = deque()
-        maxq = deque()
+        decrease_queue = collections.deque()
+        increase_queue = collections.deque()
         i = 0
-        for j in range(n):
-            while maxq and A[j] > A[maxq[-1]]:
-                maxq.pop()
-            maxq.append(j)
-            while minq and A[j] < A[minq[-1]]:
-                minq.pop()
-            minq.append(j)
+        mod = 10 ** 9 + 7
+        # i-j is a sliding window to maintain valid partition (max - min <= k)
+        # If you face 
+        for j in range(len(nums)):
+            while decrease_queue and nums[j] > decrease_queue[-1][1]:
+                decrease_queue.pop()
+            decrease_queue.append((j, nums[j]))
+            while increase_queue and nums[j] < increase_queue[-1][1]:
+                increase_queue.pop()
+            increase_queue.append((j, nums[j]))
 
-            while A[maxq[0]] - A[minq[0]] > k:
-                acc = (acc - dp[i]) % mod
+            while decrease_queue[0][1] - increase_queue[0][1] > k:
+                # you can not include partition that includes i
+                #acc = (acc - memo[i]) % mod
+                acc = (acc - (memo[i - 1] if i > 0 else 1)) % mod
+                if decrease_queue[0][0] == i:
+                    decrease_queue.popleft()
+                if increase_queue[0][0] == i:
+                    increase_queue.popleft()
                 i += 1
-                if minq[0] < i:
-                    minq.popleft()
-                if maxq[0] < i:
-                    maxq.popleft()
-            dp[j + 1] = acc
-            acc = (acc*2) % mod
-        return dp[n]        
+            # you cache the max number of partitions from 0 to j
+            memo[j] = acc
+            # why multiply by 2??
+            # In the next iteration, [x,x,x] [y]
+            acc = (acc * 2) % mod
+        print(memo)
+        return memo[-1]            
