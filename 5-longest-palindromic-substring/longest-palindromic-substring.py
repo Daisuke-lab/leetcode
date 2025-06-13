cb96b68d31842b80385f6833aae9864c8ec1ec32
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-13 14:06:53 -0500

    Time: 546 ms (39.01%) | Memory: 17.8 MB (60.52%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,33 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        original_s = s
-        s = "^#" + "#".join(s) + "#$"
-        radiuses = [0 for i in range(len(s))]
+        t = "^#" + "#".join(s) + "#$"
+        radiuses = [0 for i in range(len(t))]
         center = 0
         right = 0
-        for i in range(len(s)):
+        for i in range(len(t)):
             radius = 0
             if i < right:
                 distance = right - center
                 mirror = center - distance
-                radius = min(radiuses[mirror], right -i)
 
             l = i - (radius + 1)
             r = i + (radius + 1)
-            while l >= 0 and r < len(s) and s[l] == s[r]:
+            while l > 0 and r < len(t) and t[l] == t[r]:
                 l -= 1
                 r += 1
                 radius += 1
             radiuses[i] = radius
-            if right < r -1:
+            if r -1> right:
                 center = i
                 right = r -1
         max_radius = max(radiuses)
-        max_len = (max_radius * 2) // 2
         center_index = radiuses.index(max_radius)
-        # Convert back to original string indices
-        start = (center_index - max_len) // 2
-        return original_s[start: start + max_len]
+        start = (center_index - max_radius) // 2
+        end = (center_index + max_radius) // 2
+        return s[start:end]
              
             
         
