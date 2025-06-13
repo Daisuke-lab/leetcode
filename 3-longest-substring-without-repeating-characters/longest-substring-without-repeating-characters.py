eebfed7eb9147194fcb6d0bf24fdf0fd0b87a542
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-13 16:12:36 -0500

    Time: 11 ms (95.33%) | Memory: 17.7 MB (96.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,21 +1,14 @@
 class Solution:
-    # you can do it queue/stack
-    # visited to find duplicate
-    # 
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length = 0
         visited = set()
-        stack_queue = []
+        deque = collections.deque()
+        max_length = 0
         for c in s:
-            if c not in visited:
-                visited.add(c)
-                stack_queue.append(c)
-            else:
-                max_length = max(max_length, len(stack_queue))
-                while stack_queue[0] != c:
-                    removing_c = stack_queue.pop(0)
-                    visited.remove(removing_c)
-                stack_queue.pop(0)
-                stack_queue.append(c)
-        max_length = max(max_length, len(stack_queue))
+            while c in visited:
+                max_length = max(max_length, len(deque))
+                removing_c = deque.popleft()
+                visited.remove(removing_c)
+            deque.append(c)
+            visited.add(c)
+        max_length = max(max_length, len(deque))
         return max_length
