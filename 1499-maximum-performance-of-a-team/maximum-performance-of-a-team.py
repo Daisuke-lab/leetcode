206f4b73b01b4ed3df46eb883bf7736e0509a83b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-13 16:31:21 -0500

    Time: 99 ms (29.37%) | Memory: 39.2 MB (12.54%) - LeetSync

---
+++ 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.py
@@ -0,0 +1,16 @@
+class Solution:
+    # you can fix min efficiency by reverse sort
+    # then you keep high speed people
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        engineers = sorted(list(zip(speed, efficiency)), reverse=True, key=lambda engineer: engineer[1])
+        print(engineers)
+        min_heap = []
+        max_performance = 0
+        curr_sum = 0
+        for speed, eff in engineers:
+            heapq.heappush(min_heap, speed)
+            curr_sum += speed
+            max_performance = max(max_performance, (eff * curr_sum))
+            if len(min_heap) == k:
+                curr_sum -= heapq.heappop(min_heap)
+        return max_performance % (10**9 + 7)
