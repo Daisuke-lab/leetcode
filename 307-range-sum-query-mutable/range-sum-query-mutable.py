635c7aa7fc7950d109dc46e42f747cc6f6ae85bc
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-16 16:44:01 -0500

    Time: 458 ms (79.08%) | Memory: 36.5 MB (75.44%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/range-sum-query-mutable.py
@@ -1,52 +1,56 @@
 class SegmentTree:
     def __init__(self, nums):
         self.n = len(nums)
-        self.trees = [0] * self.n + nums
-        for i in range(self.n -1, 0, -1):
-            self.trees[i]= self.trees[i*2] + self.trees[i*2+1]
-        #print(self.trees)
+        self.sums = [0 for i in range(self.n*2)]
+        for i in range(len(self.sums) -1, 0, -1):
+            j = (i - self.n)
+            if j >= 0:
+                self.sums[i] = nums[j]
+            else:
+                left = i * 2
+                right = i * 2 + 1
+                self.sums[i] += self.sums[left]
+                self.sums[i] += self.sums[right]
+        #print(self.sums)
     
-    def update(self, i, val):
-        i += self.n
-        diff = val - self.trees[i]
-        self.trees[i] = val
-        i //= 2
+    def update(self, num, i):
+        i = self.n + i
+        gap = num - self.sums[i]
         while i > 0:
-            self.trees[i] += diff
-            i //= 2
-    
-    def sumRange(self, i, j):
+            self.sums[i] += gap
+            i = i // 2
+        
+    def get_sum_range(self, i, j):
+        curr_sum = 0
         i += self.n
         j += self.n
-        answer = 0
-        while i <= j:
-            if i % 2 == 1:
-                answer += self.trees[i]
-                i += 1
-            if j % 2 == 0:
-                answer += self.trees[j]
-                j -= 1
-            i //= 2
-            j //= 2
-        return answer
+        # for i, if it is on the left side, sum will be included in parent
+        # for j, if it is on the right side, sum will be included in parent
         
-             
+        while i <= j:
+            # right side
+            if i % 2 == 1:
+                curr_sum += self.sums[i]
+                i += 1
+            # left side
+            if j % 2 == 0:
+                curr_sum += self.sums[j]
+                j -= 1
+            i = i // 2
+            j = j // 2
+        return curr_sum
 
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.root = SegmentTree(nums)
-        #print(self.root.sum)
-        
+        self.segment_tree = SegmentTree(nums)
+
 
     def update(self, index: int, val: int) -> None:
-        self.root.update(index, val)
-        return
-        
+        self.segment_tree.update(val, index)
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.root.sumRange(left, right)
-        
+        return self.segment_tree.get_sum_range(left, right)
 
 
 # Your NumArray object will be instantiated and called as such:
