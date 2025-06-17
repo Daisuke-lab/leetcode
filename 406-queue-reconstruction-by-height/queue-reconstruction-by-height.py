b19626d9ceeb3e67e958dfc40a16d9d81087251d
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-17 17:47:01 -0500

    Time: 59 ms (17.73%) | Memory: 19.2 MB (6.79%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
@@ -1,57 +1,43 @@
-class SegmentTree:
-    def __init__(self, n):
-        self.n = n
-        self.tree = [1] * (2 * self.n)
-        for i in range(self.n - 1, 0, -1):
-            self.tree[i] = self.tree[i * 2] + self.tree[i * 2 + 1]
-
-    def update(self, i, val):
-        i += self.n
-        diff = val - self.tree[i]
-        self.tree[i] = val
-        i //= 2
-        while i > 0:
-            self.tree[i] = self.tree[i*2] + self.tree[i*2+1]
-            i //= 2
-
-    def sumRange(self, i, j):
-        answer = 0
-        i += self.n
-        j += self.n
-        while i <= j:
-            if i % 2 == 1:
-                answer += self.tree[i]
-                i += 1
-            if j % 2 == 0:
-                answer += self.tree[j]
-                j -= 1
-            i //= 2
-            j //= 2
-        return answer
+class LinkedNode():
+    def __init__(self, i=None):
+        self.i = i
+        self.prev = None
+        self.next = None
 
 class Solution:
+    # Brute Force
+    # Try every combination n!
+    
+    # start from higher height
+    # 1. sort by k
+    # 2. starts from the top, and then insert when you have k nodes before you
+    
+    # start from shorter height
+    # adding bigger number behind it, doesn't affect the rule
+    # =>  but sometimes you want to add in-between
+    
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        n = len(people)
-        # small heights and big k
-        people.sort(key=lambda x: (x[0], -x[1]))
-        print(people)
-        res = [[] for _ in range(n)]
-
-        segTree = SegmentTree(n)
-        for height, k in people:
-            l, r = 0, n - 1
-            i = 0
-            while l <= r:
-                m = (l + r) // 2
-                # when you still have space and you want to put right most position
-                # afterward, you keep putting bigger value, so position of person fixed 100%
-                if segTree.sumRange(0, m) > k:
-                    i = m
-                    r = m - 1
-                else:
-                    l = m + 1
-                    
-            res[i] = [height, k]
-            segTree.update(i, 0)
-            
-        return res
+        head = LinkedNode()
+        tail = LinkedNode()
+        head.prev = tail
+        tail.next = head
+        people.sort(key=lambda person: (-person[0], person[1]))
+        #print(people)
+        for i in range(len(people)):
+            h, k = people[i]
+            curr = tail
+            node = LinkedNode(i)
+            while k != 0:
+                curr = curr.next
+                k -= 1
+            #print(i)
+            node.prev = curr
+            node.next = curr.next
+            curr.next = node
+            curr.next.prev = node
+        answer = []
+        curr = tail.next
+        while curr != head:
+            answer.append(people[curr.i])
+            curr = curr.next
+        return answer
