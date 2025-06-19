4e71f7155ed2ccbb4312742368c33dc08af1dc46
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-19 15:00:00 -0500

    Time: 470 ms (81.46%) | Memory: 18 MB (74.15%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -1,24 +1,44 @@
 class Solution:
-    # you have two choices
-    # case1: you drop i
-    # case2: you drop i+1
-    # case3: don't drop
-    # O(n^3)
+    # you want to jump one by one
+    # you want to maintain the prev chosen value
+    
+    # when you have none in prev
+    # => you pick up current one
+    # when you have prev and it meets the requirement
+    # => you include it
+    # => you can discard curr
+    # when you have prev and it dosen't meet the requirement
+    # => you discard curr
+    # => you discard prev
 
-    # The target is O(n^2)
-    # tab = i[], {: length}
-    # what do you want??
-    # I want length at j so far and I want to add 1 for i
-    # O(n^2)
+    # base case
+    # when the last char, it's 1
+
+    # args: i, prev
+    # output: max_length
+
+    # you keep abcdefg as key and length of value
+    # check around k and pick up the max and + 1
+
+    # n*k
     def longestIdealString(self, s: str, k: int) -> int:
-        tab = [0] * 26
+        self.s = s
+        self.k = k
+        self.memo = [0 for i in range(26)]
         max_length = 0
-        for c in s:
-            i = ord(c) - ord("a")
-            range_start = max(0, i - k)
-            range_end = min(26, i + k + 1)
-            ad_max = max(tab[range_start:range_end])
-            tab[i] = 1 + ad_max
-            max_length = max(tab[i], max_length)
+        for c in self.s:
+            i = self.get_index(c)
+            length = self.get_max_length(i) + 1
+            self.memo[i] = length
+            max_length = max(max_length, length)
         return max_length
-        
+
+
+    def get_max_length(self, i):
+        start = max(i - self.k, 0)
+        end = min(i+self.k,  25)
+        max_length = max(self.memo[start:end+1])
+        return max_length
+
+    def get_index(self, c):
+        return ord(c) - ord("a")
