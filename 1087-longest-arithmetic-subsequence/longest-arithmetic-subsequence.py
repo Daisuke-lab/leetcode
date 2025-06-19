af5d475fe31eb43cf40af018dfc60c9de0dd87db
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-19 17:01:56 -0500

    Time: 2529 ms (26.47%) | Memory: 60.5 MB (28.02%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.py
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.py
@@ -1,25 +1,32 @@
 class Solution:
-    # how do you define k
+    # first two numbers are important
+    # first, second, i => O(n^3) 
     
-    ## Brute Force
-    # 1. decide gap O(n^2)
-    # 2. loop until you find current + gap number: O(n)
-    # O(n^3)
+    # calculate the gaps (n^2) 
+    # choose starting point n
+    # check it exists n
+    # O(n^4)
 
-    # flankly O(n) is impossible. let's make it O(n^2)
-    # Count gap => [20, 17, 43, 45] 25:2 
+    # hash_map
+    # {gap: count}
+    # you can always create a new gap O(n) for each
+    # if gap doesn't exist: create gap memo[gap][prev]
+    # if gap exists, 
+    # memo[gap][prev] if prev - gap == curr => increment the count and update prev
+    # memo[gap][prev] if not                => you can set new prev using curr
     def longestArithSeqLength(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n <= 2:
-            return n
-
-        longest = 2
-        dp = [{} for _ in range(n)]
-
-        for i in range(n):
-            for j in range(i):
-                diff = nums[i] - nums[j]
-                dp[i][diff] = dp[j].get(diff, 1) + 1
-                longest = max(longest, dp[i][diff])
-
-        return longest
+        memo = {}
+        max_length = 2
+        for i in range(len(nums) -1, -1, -1):
+            for j in range(i+1, len(nums)):
+                gap = nums[j] - nums[i]
+                if (gap, j) in memo:
+                    length = memo[(gap, j)] + 1
+                    max_length = max(max_length, length)
+                    if (gap, i) not in memo or memo[(gap, i)] < length:
+                        memo[(gap, i)] = length
+                else:
+                    if (gap, i) not in memo:
+                        memo[(gap, i)] = 2
+        #rint(memo)
+        return max_length
