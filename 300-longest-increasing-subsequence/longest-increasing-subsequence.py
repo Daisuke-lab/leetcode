efa74e41551cf3336b152e7cf0136cfd6a174bc1
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-18 19:03:03 -0500

    Time: 101 ms (69.12%) | Memory: 18.4 MB (9.12%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,57 +1,62 @@
 class SegmentTree:
     def __init__(self, n):
+        self.maxes = [0 for i in range(2*n)]
         self.n = n
-        self.tree = [0] * (2 * self.n)
-
-
-    def update(self, index, value):
-        # Set value at position index
-        i = index + self.n
-        self.tree[i] = value
+    def update(self, i, num):
+        i += self.n
+        self.maxes[i] = num
         i //= 2
         while i > 0:
-            self.tree[i] = max(self.tree[i*2], self.tree[i*2+1])
+            self.maxes[i] = max(self.maxes[i*2], self.maxes[i*2+1])
             i //= 2
-
-    def get_max(self, left, right):
-        # Sum on interval [left, right]
-        left += self.n
-        right += self.n
-        result = 0
-        while left <= right:
-            if left % 2 == 1:
-                result = max(result, self.tree[left])
-                left += 1
-            if right % 2 == 0:
-                result = max(result, self.tree[right])
-                right -= 1
-            left //= 2
-            right //= 2
-        return result
+    
+    def get_range_max(self, i, j):
+        i += self.n
+        j += self.n
+        max_num = 0
+        while i <= j:
+            if i % 2 == 1:
+                max_num = max(max_num, self.maxes[i])
+                i += 1
+            if j % 2 == 0:
+                max_num = max(max_num, self.maxes[j])
+                j -= 1
+            i //= 2
+            j //= 2
+        return max_num
+                 
 
 class Solution:
+    # you want the numbers that is bigger than curr => compression
+    # take the longest subsequcne out of them => segment tree
+    
     def lengthOfLIS(self, nums: List[int]) -> int:
-        def compress(arr):
-            sortedArr = sorted(set(arr))
-            order = []
-            for num in arr:
-                # binary search and find index
-                order.append(bisect_left(sortedArr, num))
-            return order
-        
-        nums = compress(nums)
+        nums = self.compress(nums)
         n = len(nums)
-        segTree = SegmentTree(n)
-
-        answer = 0
+        segment_tree = SegmentTree(n)
+        answer = 1
         for num in nums:
-            # get the max length from the numbers smaller than current
-            max_length = segTree.get_max(0, num - 1)
-            segTree.update(num, max_length+1)
-            answer = max(answer, max_length + 1)
+            max_length = segment_tree.get_range_max(0, num-1)
+            segment_tree.update(num, max_length+1)
+            answer = max(max_length + 1, answer)
+        #print(segment_tree.maxes)
         return answer
-
-        
-        
             
-        
+
+    def compress(self, nums):
+        # Convert 
+        # from [10,9,2,5,3,7,101,18]
+        # to   [5,4,0,2,1,3,7,6]
+        index_map = {}
+        i = -1
+        prev = None
+        sorted_nums = sorted(nums)
+        for num in sorted_nums:
+            if prev != num:
+                i += 1
+                prev = num
+                index_map[num] = i
+        compressed_nums = []
+        for num in nums:
+            compressed_nums.append(index_map[num])
+        return compressed_nums
