c9420d7f66b0e74ff1f3ea37e9cf51aab6cc0f6a
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-20 14:31:47 -0500

    Time: 6 ms (45.54%) | Memory: 18.5 MB (51.79%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -7,11 +7,25 @@ class Solution:
     # args: i,j *i: where you buy, j: current
     # output: max_profit
     def maxProfit(self, prices: List[int]) -> int:
-        self.prices = prices
-        self.memo = [[
-            -1 for i in range(2)]
-            for j in range(len(prices))]
-        return self.dp(0, False)
+        memo = [[
+            0 for i in range(2)]
+            for j in range(len(prices)+2)]
+        for i in range(len(prices) -1, -1, -1):
+            for has_stock in range(2):
+                result = 0
+                # when you don't have stock
+                if has_stock == False:
+                    # buy now
+                    result = max(result, memo[i+1][True] - prices[i])
+                    # don't buy
+                    result = max(result, memo[i+1][False])
+                else:
+                    # sell now
+                    result = max(result, memo[i+2][False] + prices[i])
+                    # don't sell
+                    result = max(result, memo[i+1][True])
+                memo[i][has_stock] = result
+        return memo[0][False]
 
     def dp(self, i, has_stock):
         if i >= len(self.prices):
