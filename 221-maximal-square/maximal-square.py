d9bdb353b68034a4ade5b7959da4d948321a5385
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-20 14:09:48 -0500

    Time: 203 ms (27.97%) | Memory: 27.5 MB (96.53%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,23 +1,32 @@
 class Solution:
+    # you keep matrix size memo
+    # if you are 0, it's 0
+    # if it is 1
+    # => 1. you take the min of top and left
+    # => 2. potential length would be + 1 to that
+    # => 3. check diagonal line to check if you can expand it or not
+    # get the max
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        max_length = 0
         ROW = len(matrix)
         COL = len(matrix[0])
         memo = [[
-            0 for j in range(COL)]
-            for i in range(ROW)]
-        max_length = 0
+            0 for i in range(COL)]
+            for j in range(ROW)]
         for i in range(ROW):
             for j in range(COL):
-                if matrix[i][j] == "0":
-                    continue
-                elif i == 0 or j == 0:
-                    memo[i][j] = int(matrix[i][j])
+                matrix[i][j] = int(matrix[i][j]) 
+                if i == 0 or j == 0:
+                    memo[i][j] = matrix[i][j]
+                    max_length = max(max_length, memo[i][j])
+                elif matrix[i][j] == 0:
+                    memo[i][j] = 0
                 else:
-                    ensured_length = min(memo[i-1][j], memo[i][j-1])
-                    potential_length = ensured_length + 1
-                    if matrix[i-(potential_length-1)][j-(potential_length-1)] == "1":
+                    min_length = min(memo[i-1][j], memo[i][j-1])
+                    potential_length = min_length + 1
+                    if matrix[i - potential_length + 1][j - potential_length + 1] == 1:
                         memo[i][j] = potential_length
                     else:
-                        memo[i][j] = max(1, ensured_length)
-                max_length = max(max_length, memo[i][j])
-        return max_length * max_length
+                        memo[i][j] = max(min_length, 1)
+                    max_length = max(max_length, memo[i][j])
+        return max_length**2
