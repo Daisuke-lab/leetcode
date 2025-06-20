7e1884d611f75777429b0128b177df6b924cd16c
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-20 14:25:51 -0500

    Time: 7 ms (42.83%) | Memory: 18.6 MB (51.79%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,30 +1,34 @@
 class Solution:
-    # [holdingStock (price), i] = maxProfit
-    # you can sell it but you don't know how to buy
-    # 
+    # you have 3 choices
+    # buy: 
+    # sell: once you sell, i + 2
+    # cooldown
+    
+    # args: i,j *i: where you buy, j: current
+    # output: max_profit
     def maxProfit(self, prices: List[int]) -> int:
-        self.memo = {}
         self.prices = prices
+        self.memo = [[
+            -1 for i in range(2)]
+            for j in range(len(prices))]
         return self.dp(0, False)
 
     def dp(self, i, has_stock):
         if i >= len(self.prices):
             return 0
-        elif (i, has_stock) in self.memo:
-            return self.memo[(i, has_stock)]
-        
-        profit = 0
-
-        # SELL
-        if has_stock:
-            profit = max(profit, self.dp(i+2, False) + self.prices[i])
-        # BUY
+        if self.memo[i][has_stock] != -1:
+            return self.memo[i][has_stock]
+        result = 0
+        # when you don't have stock
+        if has_stock is False:
+            # buy now
+            result = max(result, self.dp(i+1, True) - self.prices[i])
+            # don't buy
+            result = max(result, self.dp(i+1, False))
         else:
-            profit = max(profit, self.dp(i+1, True) - self.prices[i])
-
-        # if you don't do anything
-        profit = max(profit, self.dp(i+1, has_stock))
-        self.memo[(i, has_stock)] = profit
-        return profit
-        
-        
+            # sell now
+            result = max(result, self.dp(i+2, False) + self.prices[i])
+            # don't sell
+            result = max(result, self.dp(i+1, True))
+        self.memo[i][has_stock] = result
+        return result
