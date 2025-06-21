926070cbd5787fb77fced67d7f54fa8c45d1a447
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-21 18:54:36 -0500

    Time: 471 ms (43.21%) | Memory: 26.9 MB (93.29%) - LeetSync

--- 3841-concatenated-divisibility/concatenated-divisibility.py
+++ 3841-concatenated-divisibility/concatenated-divisibility.py
@@ -1,34 +1,32 @@
 class Solution:
-    # visited, curr, 
-    
+    # args: visited, reminder, curr
+    # how do you get the list as output
+    # you can pass it as arg but you don't have to cache
     def concatenatedDivisibility(self, nums: List[int], k: int) -> List[int]:
+        self.nums = sorted(nums)
         self.k = k
-        self.nums = nums
-        self.all_visited = (1 << len(nums)) - 1
-        self.power = [10**len(str(num)) % k for num in nums]
-        self.indices = sorted(range(len(nums)), key=lambda i: nums[i])
+        self.all_visited = (1 << len(nums)) -1
+        self.answer = []
         self.memo = [[
-            -1 for i in range(k)]
-            for j in range(self.all_visited)]
-            
-        result = self.dp(0, 0)
-        return result if result is not None else []
-
-    def dp(self, visited, rem):
-        if self.all_visited == visited:
-            return [] if rem == 0 else None
-        if self.memo[visited][rem] != -1:
-            return self.memo[visited][rem]
-        result = None
-        for i in self.indices:
-            unvisited = (visited & (1 << i)) == 0
+            -1 for a in range(k)]
+            for b in range(self.all_visited + 1)]
+        self.dp(0, 0, [])
+        return self.answer
+    def dp(self, visited, reminder, curr):
+        if visited == self.all_visited:
+            if reminder == 0:
+                self.answer = curr.copy()
+        if len(self.answer) > 0:
+            return
+        if self.memo[visited][reminder] != -1:
+            return 
+        for i in range(len(self.nums)):
+            unvisited = ((1 << i) & visited) == 0
             if unvisited:
-                new_visited = visited | (1 << i)
-                new_rem = (rem * self.power[i] + self.nums[i]) % self.k
-                new_rem = int(str(rem) + str(self.nums[i])) % self.k
-                result = self.dp(new_visited, new_rem)
-                if result is not None:
-                    result = [self.nums[i]] + result
-                    break
-        self.memo[visited][rem] = result
-        return result   
+                new_visited = (1 << i) | visited
+                new_reminder = int(str(reminder) + str(self.nums[i])) % self.k 
+                curr.append(self.nums[i])
+                self.dp(new_visited, new_reminder, curr)
+                del curr[-1]
+        self.memo[visited][reminder] = 0
+        
