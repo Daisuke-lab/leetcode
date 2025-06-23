61837fd5c79292765717f94ecf50dea45d85d408
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-23 15:29:41 -0500

    Time: 735 ms (5.71%) | Memory: 54.5 MB (5.14%) - LeetSync

--- 1471-maximum-students-taking-exam/maximum-students-taking-exam.py
+++ 1471-maximum-students-taking-exam/maximum-students-taking-exam.py
@@ -1,45 +1,54 @@
 class Solution:
-    # DFS
-    # taken seat is [(i, j)]
-    # => Because a student can only see the front row, it can only be j.
-    # First, you need to generate all the seat allocation patterns in the row
-    # n/3 permutation => sets
-    # m^2 * n *  
+    # you want to pass row as bitmask that shows availability
+    # how do you handle different student alignment in current row.
+    # => you can also pass i and j 
+    # when you have available seat
+    # case1: you skip it and i + 1
+    # case2: take a seat and i + 2
+    # => you need two bitmasks (curr, next)
+    # O(2^n * 2^n * n * m)
+    # args: i, j, bitmask
+    # output: the max number of students
     def maxStudents(self, seats: List[List[str]]) -> int:
+        self.seats = seats
         self.ROW = len(seats)
         self.COL = len(seats[0])
         self.all_seated = (1 << self.COL) - 1
-        self.seats = seats
-        self.memo = [[
-            -1 for i in range(self.ROW)]
-                for j in range(self.all_seated)]
-        return self.dp(0, 0)
-        
-    def dp(self, front_seated, i):
-        if i == self.ROW:
-            return 0
-        if self.memo[front_seated][i] != -1:
-            return self.memo[front_seated][i]
-        row = self.seats[i]
-        seat_patterns = set()
-        seat_patterns.add((0, 0))
-        for j in range(self.COL):
-            if self.seats[i][j] == "#":
-                continue
-            upper_left_seated = (front_seated & 1 << j - 1) != 0 if j != 0 else False
-            upper_right_seated = (front_seated & 1 << j + 1) != 0 if j != self.COL - 1 else False
-            if not upper_left_seated and not upper_right_seated:
-                new_seat_patterns = set()
-                for seat_pattern, count in seat_patterns:
-                    new_seat_patterns.add((seat_pattern, count))
-                    left_seated = (seat_pattern & 1 << j - 1) != 0 if j != 0 else False
-                    right_seated = (seat_pattern & 1 << j + 1) != 0 if j != self.COL - 1 else False
-                    if not left_seated and not right_seated:
-                        new_seat_pattern = seat_pattern | 1 << j
-                        new_seat_patterns.add((new_seat_pattern, count + 1))
-                seat_patterns = new_seat_patterns
-        result = 0
-        for seat_pattern, count in seat_patterns:
-            result = max(result, self.dp(seat_pattern, i+1) + count)
-        self.memo[front_seated][i] = result
+        self.memo = [[[[
+            -1 for a in range(self.all_seated + 1)]
+            for b in range(self.all_seated + 1)]
+            for c in range(self.COL)]
+            for d in range(self.ROW)]
+        result =  self.dp(0, 0, 0, 0)
         return result
+
+
+    def dp(self, i, j, curr_seated, invalid_seats):
+        if i >= self.ROW:
+            return 0
+        elif j >= self.COL:
+            new_invalid_seats = 0
+            for j in range(self.COL):
+                if self.is_next_unavailable(j, curr_seated):
+                    new_invalid_seats = (new_invalid_seats | (1 << j))
+            return self.dp(i+1, 0, 0, new_invalid_seats)
+        elif self.memo[i][j][curr_seated][invalid_seats] != -1:
+            return self.memo[i][j][curr_seated][invalid_seats]
+
+        invalid = (invalid_seats & (1 << j)) != 0
+        result = 0
+        if self.seats[i][j] == "#" or invalid:
+            result = self.dp(i, j+1, curr_seated, invalid_seats)
+        else:
+            new_curr_seated = (curr_seated | (1 << j))
+            result = max(result, self.dp(i, j+2, new_curr_seated, invalid_seats) + 1)
+            result = max(result, self.dp(i, j+1, curr_seated, invalid_seats))
+        self.memo[i][j][curr_seated][invalid_seats] = result
+        return result
+
+    def is_next_unavailable(self, j, curr_seated):
+        if ((1 << j+1) & curr_seated) != 0:
+            return True
+        if j > 0 and ((1 << j-1) & curr_seated) != 0:
+            return True
+        return False
