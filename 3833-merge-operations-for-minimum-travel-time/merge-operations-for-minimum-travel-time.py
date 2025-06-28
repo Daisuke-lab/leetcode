74c70eca8445edc471ca7b4ce501c5666bfb0711
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-28 14:42:05 -0500

    Time: 351 ms (23.47%) | Memory: 18.2 MB (92.50%) - LeetSync

--- 3833-merge-operations-for-minimum-travel-time/merge-operations-for-minimum-travel-time.py
+++ 3833-merge-operations-for-minimum-travel-time/merge-operations-for-minimum-travel-time.py
@@ -5,60 +5,41 @@ class Solution:
     # if you merge => you use sum of time as speed
     # if you don't merge => you use individual time as speed
 
-    # When you merge, where is happening to combine time
+    # 
     def minTravelTime(self, l: int, n: int, k: int, positions: List[int], times: List[int]) -> int:
-        self.prefix_sums = self.get_prefix_sums(times)
         self.positions = positions
         self.times = times
         self.n = n
         self.memo = [[[
-            -1 for p in range(k+1)]
-                for j in range(n)]
+            -1 for a in range(101)]
+            for p in range(k+1)]
                 for i in range(n)]
-        return self.dp(0, 0, k, times[0])
+        result = self.dp(0, k, times[0])
+        return result
     
-    def dp(self, i, j, k, speed):
-        #print(i, j, k)
-        if j == len(self.positions) -1:
+    def dp(self, i, k, speed):
+        if i >= len(self.positions)-1:
             if k == 0:
                 return 0
             else:
                 return float("inf")
-        if self.memo[i][j][k] != -1:
-            return self.memo[i][j][k]
+        #print(i, k, speed)
+        if self.memo[i][k][speed] != -1:
+            return self.memo[i][k][speed]
 
 
         min_cost = float("inf")
         original_k = k
         next_speed = 0
-        next_i = j + 1
-        # because you seperate merge, 0,3, you don't nerge 3, 8. that will be (i, j) = 0, 2
-        for next_j in range(j+1, self.n):
+        for j in range(i+1, self.n):
             if k < 0:
                 break
-            distance = self.positions[next_j] - self.positions[j]
-            next_speed += self.times[next_j]
+            distance = self.positions[j] - self.positions[i]
+            next_speed += self.times[j]
             # In the first iteration, it'd be self.dp(j+1, j+1, k) => when you don't merge i to j
-            cost = distance * speed + self.dp(next_i, next_j, k, next_speed)
+            cost = distance * speed + self.dp(j, k, next_speed)
             min_cost = min(min_cost, cost)
             k-= 1
         k = original_k
-        self.memo[i][j][k] = min_cost
+        self.memo[i][k][speed] = min_cost
         return min_cost
-
-        
-
-        
-        
-
-
-
-    
-    def get_prefix_sums(self, nums):
-        prefix_sums = []
-        for num in nums:
-            if prefix_sums:
-                prefix_sums.append(num + prefix_sums[-1])
-            else:
-                prefix_sums.append(num)
-        return prefix_sums
