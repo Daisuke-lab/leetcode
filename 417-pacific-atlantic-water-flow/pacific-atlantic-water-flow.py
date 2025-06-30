eeec20de5b0c8c17bf5060ec5dabda1c30cf39d9
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-30 14:28:13 -0500

    Time: 27 ms (84.68%) | Memory: 19.2 MB (91.76%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,52 +1,49 @@
 class Solution:
-    # DP
-    # you can mark the position as pacific_reachable, atlantic_reachable
-    # once you reach the cell, it means you don't have to go through it again
-    # you go through every poisition and check if it can be reachable to both oceans
-    # Let's define another function
-    # the output should be pacific_reachable, atlantic_reachable
-    # before propagating, please check the height and index range
-    # args would be (i, j)
-    # you can cache tab[i][j] = pacific_reachable, atlantic_reachable
-
-    # when it's the same height, the consequence changes based on visited.
-    # but at the same time, you want to prevent infinite loop
-    # If it's the same height, you can pass ad_reachable as args
+    # from the edge to the top
+    # you keep expanding it as long as 
+    # it is higher than or equal to prev
+    # it is not labeled yet
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         self.heights = heights
-        self.ROW = len(self.heights)
-        self.COL = len(self.heights[0])
-        pacific_reachable = set()
-        atlantic_reachable = set()
-
-        for j in range(self.COL):
-            first = 0
-            last = self.ROW -1
-            self.dig(first, j, pacific_reachable)
-            self.dig(last, j, atlantic_reachable)
+        self.ROW = len(heights)
+        self.COL = len(heights[0])
+        self.memo = [[
+            0 for i in range(self.COL)]
+            for j in range(self.ROW)]
+        self.answer = []
         for i in range(self.ROW):
-            first = 0
-            last = self.COL - 1
-            self.dig(i, first, pacific_reachable)
-            self.dig(i, last, atlantic_reachable)
-        answer = []
-        for i, j in pacific_reachable:
-            if (i, j) in atlantic_reachable:
-                answer.append([i, j])
-        return answer
+            self.flow(i, 0, -1, True)
+            self.flow(i, self.COL - 1, -1, False)
+        for j in range(self.COL):
+            self.flow(0, j, -1, True)
+            self.flow(self.ROW - 1, j, -1, False)
+        return self.answer
 
-    def dig(self, i, j, visited):
-        if (i, j) in visited:
+
+    def flow(self, i, j, prev, is_pacific):
+        if i < 0 or j < 0 or i >= self.ROW or j >= self.COL:
             return 
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        visited.add((i, j))
-        for direction in directions:
-            new_i = i + direction[0]
-            new_j = j + direction[1]
-            if new_i < 0 or new_i == self.ROW or new_j < 0 or new_j == self.COL:
-                continue 
-            if self.heights[i][j] > self.heights[new_i][new_j]:
-                continue
-            self.dig(new_i, new_j, visited)
+        elif prev > self.heights[i][j]:
+            return
+        elif self.memo[i][j] == 3:
+            return
+        elif is_pacific and self.memo[i][j] == 1:
+            return
+        elif not is_pacific and self.memo[i][j] == 2:
+            return
+        
+        if self.memo[i][j] == 0:
+            self.memo[i][j] = 1 if is_pacific else 2
+        elif self.memo[i][j] == 1 and not is_pacific:
+            self.memo[i][j] = 3
+            self.answer.append([i, j])
+        elif self.memo[i][j] == 2 and is_pacific:
+            self.memo[i][j] = 3
+            self.answer.append([i, j])
+        self.flow(i-1, j, self.heights[i][j], is_pacific)
+        self.flow(i+1, j, self.heights[i][j], is_pacific)
+        self.flow(i, j-1, self.heights[i][j], is_pacific)
+        self.flow(i, j+1, self.heights[i][j], is_pacific)
 
         
+
