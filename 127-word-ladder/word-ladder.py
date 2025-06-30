54e6246a23efd092203b4b1ed22951bee377a4c2
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-06-30 15:48:57 -0500

    Time: 102 ms (61.12%) | Memory: 31.7 MB (5.10%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,45 +1,33 @@
 class Solution:
+    # h*t
+    # create ad_list
+    # dijkstra
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
-        if beginWord not in wordList:
-            wordList.append(beginWord)
-        self.ad_list = self.init_ad_list(wordList)
-        self.dj_map = self.init_dj_map(self.ad_list, beginWord)
-        min_heap = [(1, beginWord)]
+        self.words = wordList + [beginWord]
+        self.ad_list = self.init_ad_list(self.words)
+        queue = collections.deque()
+        queue.append((1, beginWord))
         visited = set()
-        while min_heap:
-            distance, word = heapq.heappop(min_heap)
+        while queue:
+            count, word = queue.popleft()
             if word in visited:
                 continue
+            if word == endWord:
+                return count
             visited.add(word)
-            for ad in self.ad_list[word]:
-                if self.dj_map[ad] > 1 + distance:
-                    self.dj_map[ad] = 1 + distance
-                if ad not in visited:
-                    heapq.heappush(min_heap, (distance + 1, ad))
-        return self.dj_map[endWord] if self.dj_map[endWord] != float("inf") else 0
-
-    def init_dj_map(self, ad_list, beginWord):
-        # {node: distance}
-        dj_map = {}
-        for node in ad_list:
-            dj_map[node] = float("inf")
-        dj_map[beginWord] = 1
-        return dj_map
-
-    def init_ad_list(self, wordList):
-        ad_list = {}
-        words = set(wordList)
-        for word in wordList:
             for i in range(len(word)):
-                for c in "qwertyuiopasdfghjklzxcvbnm":
-                    ad = f"{word[:i]}{c}{word[i+1:]}"
-                    ads = ad_list.get(word, set())
-                    if ad in words:
-                        ads.add(ad)
-                    ad_list[word] = ads
-        return ad_list
-                
+                regex = word[:i] + "*" + word[i+1:]
+                for ad in self.ad_list[regex]:
+                    queue.append((count+1, ad))
+        return 0
 
-        
+
+    def init_ad_list(self, words):
+        ad_list = {}
+        for word in words:
+            for i in range(len(word)):
+                regex = word[:i] + "*" + word[i+1:]
+                ads = ad_list.get(regex, set())
+                ads.add(word)
+                ad_list[regex] = ads
+        return ad_list
