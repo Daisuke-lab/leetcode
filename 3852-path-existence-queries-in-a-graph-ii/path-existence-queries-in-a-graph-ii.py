9236f9ec081408253c0931c6d45aade5dfba1834
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-07-01 15:58:50 -0500

    Time: 2298 ms (28.49%) | Memory: 87.2 MB (58.49%) - LeetSync

--- 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.py
+++ 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.py
@@ -1,7 +1,6 @@
 class Solution:
-    # If you know it's not in the same component, it immediately can return -1
-    # 
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
+        # The reason why you want to sort it, is because you want to speed up binary_lifting cnostruction
         nums = self.get_sorted_nums(nums)
         self.ancestors = self.build_binary_lifting(n, nums, maxDiff)
         
@@ -9,57 +8,53 @@ class Solution:
         for src, dest in queries:
             src = self.get_curr_index(src)
             dest = self.get_curr_index(dest)
-            curr = self.query(min(src, dest), max(src, dest))
-            distances.append(curr if curr < float("inf") else -1)
+            distance = self.query(min(src, dest), max(src, dest))
+            distances.append(distance)
         return distances
 
 
-    @cache   
-    def query(self, src, dest, jump=None):
-        jump = jump if jump is not None else self.lift_height - 1
+    def query(self, src, dest):
+        #print("SRC:", src, "DEST:", dest)
         if src == dest:
             return 0
-        # if src is reachable to bigger number than dest
+        steps = 0
+        for exponent in range(self.max_exponent, -1, -1):
+            if self.ancestors[src][exponent] != -1 and self.ancestors[src][exponent] < dest:
+                src = self.ancestors[src][exponent]
+                steps += 2**exponent
+            if src >= dest:
+                break
         if self.ancestors[src][0] >= dest:
-            return 1
-        # if farthest from src is smaller than dest
-        if self.ancestors[src][jump] < dest:
-            return float("inf")
-        # find spot just before it exceeds dest
-        for next_jump in range(jump, -1, -1):
-            if self.ancestors[src][next_jump] < dest:
-                break 
-        steps = 1 << next_jump
-        next_src = self.ancestors[src][next_jump]
-        return steps + self.query(next_src, dest, next_jump)
+            steps += 1
+            src = self.ancestors[src][0]
+        return steps if src >= dest else -1
 
     
     def build_binary_lifting(self, n, nums, maxDiff):
-        self.lift_height = ceil(math.log(n, 2)) + 1
+        #print(nums)
+        self.max_exponent = ceil(math.log(n, 2))
         ancestors = [[
-            -1 for j in range(self.lift_height)]
+            -1 for j in range(self.max_exponent + 1)]
             for i in range(n)]
 
-        # for i in range(n):
-        #     for j in range(i, n):
-        #         # insert the the biggest neighbor as parents
-        #         if nums[j] - nums[i] <= maxDiff:
-        #             ancestors[i][0] = j
-        #         else:
-        #             break
         j = 0
         for i in range(n):
+            # insert the the biggest neighbor as parents
             while j+1 < n and nums[j+1] - nums[i] <= maxDiff:
                 j += 1
-            ancestors[i][0] = j
+            if i != j:
+                ancestors[i][0] = j
+        for exponent in range(self.max_exponent+1):
+            ancestors[-1][exponent] = -1
 
-        for jump in range(1, self.lift_height):
+        for exponent in range(self.max_exponent+1):
             for node in range(n):
-                ancestors[node][jump] = ancestors[ancestors[node][jump-1]][jump-1]
+                # farest place with 2**jump from node = farest place with 2**jump-1 from 2**jump-1 ahead node from current
+                prev = ancestors[node][exponent-1]
+                if prev != -1:
+                    ancestors[node][exponent] = ancestors[prev][exponent-1]
+        #print(ancestors)
         return ancestors
-        
-
-            
 
     def get_sorted_nums(self, nums):
         nums_indices = sorted([(num, i) for i, num in enumerate(nums)])
