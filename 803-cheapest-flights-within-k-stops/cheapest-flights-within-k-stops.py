ec4a8a0d5461b8bc894a9dab69a27e7bb4d1ed66
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-07-01 13:57:31 -0500

    Time: 2 ms (93.99%) | Memory: 19.4 MB (28.30%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,22 +1,30 @@
 class Solution:
+    # k = 0 => 1
+    # k = 1 => 2, 3
+    # k = 2 => 3
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        self.ad_list = self.init_ad_list(n, flights)
-        bf_map = [float("inf") for i in range(n)]
-        bf_map[src] = 0
-        for _k in range(k + 1):
-            new_bf_map = bf_map.copy()
-            for node in range(n):
-                # when you haven't arrived node in _k time yet
-                if bf_map[node] == float("inf"):
-                    continue
-                for price, ad in self.ad_list[node]:
-                    if new_bf_map[ad] > bf_map[node] + price:
-                        new_bf_map[ad] = bf_map[node] + price
-            bf_map = new_bf_map
-        return bf_map[dst] if bf_map[dst] != float("inf") else -1
+        visited = set()
+        queue = collections.deque()
+        queue.append(src)
+        ad_list = self.init_ad_list(n, flights)
+        prices = [float("inf") for i in range(n)]
+        prices[src] = 0
+        while k >= 0:
+            count = len(queue)
+            k -= 1
+            next_prices = prices.copy()
+            #print(queue)
+            for i in range(count):
+                node = queue.popleft()
+                for ad, flight_price in ad_list[node]:
+                    if prices[node] + flight_price < next_prices[ad]:
+                        next_prices[ad] = prices[node] + flight_price
+                        queue.append(ad)
+            prices = next_prices
+        return prices[dst] if prices[dst] != float("inf") else -1 
 
-    def init_ad_list(self, n, flights):
-        ad_list = [set() for i in range(n)]
-        for src, dest, price in flights:
-            ad_list[src].add((price, dest))
-        return ad_list      
+    def init_ad_list(self, n, edges):
+        ad_list = {i: set() for i in range(n)}
+        for v1, v2, price in edges:
+            ad_list[v1].add((v2, price))
+        return ad_list
