45087601522f7fc09e7da1f4cc26abafc32f238b
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-07-02 13:21:51 -0500

    Time: 67 ms (32.13%) | Memory: 32.5 MB (5.58%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,43 +1,29 @@
 class Solution:
+    # init ad_list
+    # collected <= n - 2
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         self.ad_list = self.init_ad_list(n, edges)
-        # self.memo[i][0]: tallest subtree height from node i (max height from children)
-        # self.memo[i][1]: second tallest subtree height from node i
-        self.memo = [[0] * 2 for _ in range(n)]
-        self.build_heights_to(0)
-        self.build_heights_from(0)
-        min_height = min([self.memo[i][0] for i in range(n)])
-        answer = [i for i in range(n) if self.memo[i][0] == min_height]
-        return answer
-        
-    def build_heights_to(self, node, parent=None):
-        for ad in self.ad_list[node]:
-            if ad == parent:
-                continue
-            height = self.build_heights_to(ad, node) + 1
-            if height > self.memo[node][0]:
-                self.memo[node][1] = self.memo[node][0]
-                self.memo[node][0] = height
-            elif height > self.memo[node][1]:
-                self.memo[node][1] = height
-        return self.memo[node][0]
+        collected = set()
+        one_incomings = self.get_one_incomings(self.ad_list)
+        while one_incomings and len(collected) < n - 2:
+            count = len(one_incomings)
+            for i in range(count):
+                node = one_incomings.popleft()
+                if node in collected:
+                    continue
+                collected.add(node)
+                for ad in self.ad_list[node]:
+                    self.ad_list[ad].remove(node)
+                    if len(self.ad_list[ad]) == 1:
+                        one_incomings.append(ad)
+        return list(one_incomings)
 
-
-    def build_heights_from(self, node, parent=None, curr_height=0):
-        if curr_height > self.memo[node][0]:
-            self.memo[node][1] = self.memo[node][0]
-            self.memo[node][0] = curr_height
-        elif curr_height > self.memo[node][1]:
-            self.memo[node][1] = curr_height
-
-        for ad in self.ad_list[node]:
-            if ad == parent:
-                continue
-            if self.memo[node][0] == 1 + self.memo[ad][0]:
-                next_height = 1 + self.memo[node][1]
-            else:
-                next_height = 1 + self.memo[node][0]
-            self.build_heights_from(ad, node, next_height)
+    def get_one_incomings(self, ad_list):
+        one_incomings = collections.deque()
+        for node in ad_list.keys():
+            if len(ad_list[node]) <= 1:
+                one_incomings.append(node)
+        return one_incomings
 
 
     def init_ad_list(self, n, edges):
@@ -46,6 +32,4 @@ class Solution:
             ad_list[v1].add(v2)
             ad_list[v2].add(v1)
         return ad_list
-        
 
-        
