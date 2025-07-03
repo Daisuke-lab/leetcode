2efe9598b55d69c52b2902fced4533db30eaf37a
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-07-03 13:51:50 -0500

    Time: 156 ms (91.06%) | Memory: 18.9 MB (54.47%) - LeetSync

--- 949-cat-and-mouse/cat-and-mouse.py
+++ 949-cat-and-mouse/cat-and-mouse.py
@@ -1,75 +1,86 @@
-from collections import deque
-
 CAT_TURN = 0
 MOUSE_TURN = 1
+CAT = "CAT"
+MOUSE = "MOUSE"
 class Solution:
     def catMouseGame(self, graph: List[List[int]]) -> int:
         n = len(graph)
+        self.n = n
         ad_list = graph
-        # The state is (m, c, turn), where turn 0 is mouse's turn, 1 is cat's turn
-        # We will use BFS starting from known terminal states
-        # The color (result) of each state: 0 (draw), 1 (mouse wins), 2 (cat wins)
-        color = [[[0] * 2 for _ in range(n)] for __ in range(n)]
-        # The degree is the number of possible moves from this state for mouse or cat
-        degree = [[[0] * 2 for _ in range(n)] for __ in range(n)]
+        self.ad_list = ad_list
+        self.degrees = self.init_degrees(n, ad_list)
+        queue, self.memo = self.init_memo(n)
+        while queue:
+            mouse, cat, prev_turn = queue.popleft()
+            if (mouse, cat, prev_turn) == (1, 2, CAT_TURN):
+                return self.memo[mouse][cat][prev_turn]
+            self.collect_new_fixed_locations(mouse, cat, prev_turn, queue)
+        return 0
+
+    def collect_new_fixed_locations(self, prev_mouse, prev_cat, prev_turn, queue):
+        curr_turn = self.get_curr_turn(prev_turn)
+        prev_result = self.memo[prev_mouse][prev_cat][prev_turn]
+        if prev_turn == MOUSE_TURN:
+            for mouse in self.ad_list[prev_mouse]:
+                if self.memo[mouse][prev_cat][CAT_TURN] != 0:
+                    continue
+                elif prev_result == 1:
+                    self.memo[mouse][prev_cat][CAT_TURN] = 1
+                    queue.append((mouse, prev_cat, CAT_TURN))
+                elif prev_result == 2:
+                    self.degrees[mouse][prev_cat][CAT_TURN] -= 1
+                
+                    if self.degrees[mouse][prev_cat][CAT_TURN] == 0:
+                        self.memo[mouse][prev_cat][CAT_TURN] = 2
+                        queue.append((mouse, prev_cat, CAT_TURN))
+
+        elif prev_turn == CAT_TURN:
+            for cat in self.ad_list[prev_cat]:
+                if cat == 0:
+                    continue
+                if self.memo[prev_mouse][cat][MOUSE_TURN] != 0:
+                    continue
+                elif prev_result == 2:
+                    self.memo[prev_mouse][cat][MOUSE_TURN] = 2
+                    queue.append((prev_mouse, cat, MOUSE_TURN))
+                elif prev_result == 1:
+                    self.degrees[prev_mouse][cat][MOUSE_TURN] -= 1
+                    if self.degrees[prev_mouse][cat][MOUSE_TURN] == 0:
+                        self.memo[prev_mouse][cat][MOUSE_TURN] = 1
+                        queue.append((prev_mouse, cat, MOUSE_TURN))
+                
         
+    def get_curr_turn(self, prev_turn):
+        return MOUSE_TURN if prev_turn == CAT_TURN else CAT_TURN
+
+
+    def init_memo(self, n):
+        queue = collections.deque()
+        memo = [[[
+            0 for a in range(2)]
+            for b in range(n)]
+            for c in range(n)]
+        for cat in range(n):
+            for turn in range(2):
+                memo[0][cat][turn] = 1
+                queue.append((0, cat, turn))
+                if cat != 0:
+                    memo[cat][cat][turn] = 2
+                    queue.append((cat, cat,turn))
+        return queue, memo
+
+
+    def init_degrees(self, n, ad_list):
+        degrees = [[[
+            0 for a in range(2)]
+            for b in range(n)]
+            for c in range(n)]
+            
         for mouse in range(n):
             for cat in range(n):
-                degree[mouse][cat][CAT_TURN] = len(ad_list[mouse])  # mouse's turn, can move to any adjacent node
-                degree[mouse][cat][MOUSE_TURN] = len(ad_list[cat]) - (0 in ad_list[cat])  # cat's turn, can't move to 0
-        
-        queue = collections.deque()
-        # Initialize terminal states
-        for cat in range(n):
-            for state in range(2):
-                # Mouse wins if it's at 0
-                color[0][cat][state] = 1
-                queue.append((0, cat, state))
-                # Cat wins if cat and mouse are at the same position (not 0)
-                if cat != 0:
-                    color[cat][cat][state] = 2
-                    queue.append((cat, cat, state))
-        
-        while queue:
-            mouse, cat, state = queue.popleft()
-            result = color[mouse][cat][state]
-            is_cat_turn = state == 0
-            is_mouse_turn = not is_cat_turn
-            # this is the answer
-            if (mouse, cat, state) == (1, 2, 0):
-                return result
-            # Get all parent states that can lead to this state
-            if is_cat_turn:  # current turn is mouse's, previous turn was cat's
-                for next_cat in ad_list[cat]:
-                    if next_cat == 0:
-                        continue
-                    # if already visited, continue
-                    if color[mouse][next_cat][MOUSE_TURN] != 0:
-                        continue
-                    # if current result is 2, and after mouse finished moving and position itself at "mouse"
-                    # cat can reach out from next_cat to current "cat" position
-                    if result == 2:
-                        color[mouse][next_cat][MOUSE_TURN] = 2
-                        queue.append((mouse, next_cat, MOUSE_TURN))
-                    # if there is a selection that leads to mouse's win, you decrease the degree by 1
-                    elif result == 1:
-                        degree[mouse][next_cat][MOUSE_TURN] -= 1
-                        # once it's guaranteed there is no more option (no degree), you mark it as 1
-                        if degree[mouse][next_cat][MOUSE_TURN] == 0:
-                            color[mouse][next_cat][MOUSE_TURN] = 1
-                            queue.append((mouse, next_cat, MOUSE_TURN))
-            elif is_mouse_turn:  
-                for next_mouse in ad_list[mouse]:
-                    if color[next_mouse][cat][CAT_TURN] != 0:
-                        continue
-                    if result == 1:
-                        color[next_mouse][cat][CAT_TURN] = 1
-                        queue.append((next_mouse, cat, CAT_TURN))
-                    elif result == 2:
-                        degree[next_mouse][cat][CAT_TURN] -= 1
-                        if degree[next_mouse][cat][CAT_TURN] == 0:
-                            color[next_mouse][cat][CAT_TURN] = 2
-                            queue.append((next_mouse, cat, CAT_TURN))
-        
-        # if it is not appended to the queue (neither 1 nor 2), it's 0
-        return 0
+                # when positions are mouse, cat and cat is done for moving, how many choices mouse have.
+                degrees[mouse][cat][CAT_TURN] = len(ad_list[mouse])
+                # when positions are mouse, cat and mouse is done for moving. how many choices cat have.
+                degrees[mouse][cat][MOUSE_TURN] = len(ad_list[cat]) - (ad_list[cat].count(0))
+                
+        return degrees
