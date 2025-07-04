f402ffc6dafe976909bee0cdf6df83ef0da2ebfe
Daisuke Kikuchi <66515478+Daisuke-lab@users.noreply.github.com>
2025-07-04 14:31:22 -0500

    Time: 8102 ms (64.17%) | Memory: 62.6 MB (24.33%) - LeetSync

--- 3913-partition-array-to-minimize-xor/partition-array-to-minimize-xor.py
+++ 3913-partition-array-to-minimize-xor/partition-array-to-minimize-xor.py
@@ -1,23 +1,21 @@
 class Solution:
     def minXor(self, nums: List[int], k: int) -> int:
+        
         n = len(nums)
+        @cache
+        def dp(index, parts):
+            if parts == 0 and index == n:
+                return 0
+            if parts == 0 or index == n:
+                return float('inf')
 
-        # Step 1: Prefix XOR
-        pfix = [0] * (n + 1)
-        for i in range(1, n + 1):
-            pfix[i] = pfix[i - 1] ^ nums[i - 1]
+            current = 0
+            answer = float('inf')
 
-        # Step 2: DP table
-        dp = [[float('inf')] * (k + 1) for _ in range(n + 1)]
-        for i in range(n + 1):
-            dp[i][1] = pfix[i]  # Base case: 1 partition
-
-        # Step 3: Fill DP
-        for parts in range(2, k + 1):
-            for end in range(parts, n + 1):
-                for split in range(parts - 1, end):
-                    segmentXOR = pfix[end] ^ pfix[split]
-                    maxXOR = max(dp[split][parts - 1], segmentXOR)
-                    dp[end][parts] = min(dp[end][parts], maxXOR)
-
-        return dp[n][k]
+            for i in range(index, n - (parts-1)):
+                current ^= nums[i]
+                next = dp(i + 1, parts - 1)
+                maximum = max(current,next)
+                answer = min(answer, maximum)
+            return answer
+        return dp(0,k)
